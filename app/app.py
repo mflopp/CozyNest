d1870425e91987badaa3d51740d02212fde04526
Sam Malikin <workmalikin@gmail.com>
2024-11-29 20:02:07 +0200

    Updated reqs and syntax

--- app/app.py
+++ app/app.py
@@ -7,30 +7,33 @@ from config import init_db, setup_logger, load_config
 def start_app():
     app = Flask(__name__)
     try:
-        
+
         init_db()
         setup_logger()
-        
+
         # register_error_handlers(app)
-                
+
         app.config.update(load_config())
-        
+
     except Exception as e:
         logging.critical(f"Failed at initialization: {str(e)}")
-        
-    # main 
+
+    # main
     @app.route('/', methods=["GET"])
     def index():
-        return "Hello CozyNest!", 200   
-    
+        return "Hello CozyNest!", 200
+
     # Catch-all route
     @app.route('/<path:path>', methods=["GET", "POST", "PUT", "DELETE"])
     def catch_all(path):
         return "Route not found", 404
-    
+
     return app
+
 
 if __name__ == '__main__':
     app = start_app()
     logging.info("Starting Flask app")
-    app.run(debug=app.config["DEBUG"], port=app.config["PORT"], host=app.config["HOST"])
+    app.run(debug=app.config["DEBUG"],
+            port=app.config["PORT"],
+            host=app.config["HOST"])
