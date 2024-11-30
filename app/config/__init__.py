4a47e8e94228f9a0743722744f9227892104d5c9
Sam Malikin <workmalikin@gmail.com>
2024-11-30 19:39:26 +0200

    Update __init__.py

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,34 +1,3 @@
-from .db import init_db, get_db
-import logging
-import os
-from dotenv import load_dotenv
-
-def setup_logger(log_level=logging.INFO):
-    """
-    Setup basic logging configuration.
-    """
-    logging.getLogger('werkzeug').setLevel(logging.ERROR)
-    logging.basicConfig(
-        level=log_level,
-        format="%(levelname)s - %(message)s - %(asctime)s",
-        datefmt="%H:%M:%S"
-    )
-
-def load_config():
-    """
-    Load configuration from environment variables.
-    """
-    load_dotenv() # Load .env file into environment variables
-    if not os.getenv("SECRET_KEY"):
-        logging.warning("SECRET_KEY isn't found in the environment, using the default")
-
-    if not os.getenv("CLIENT_URL"):
-        logging.warning("CLIENT_URL isn't found in the environment, using the default")
-    return {
-        "SECRET_KEY": os.getenv("SECRET_KEY", "default_secret"),
-        "DEBUG": os.getenv("DEBUG", "false").lower() == "true",
-        "CLIENT_URL": os.getenv("CLIENT_URL", "*"),
-        "PORT": int(os.getenv("PORT", 5000)),
-        "HOST": os.getenv("HOST", "0.0.0.0"),
-    }
-
+from .base_config import Base
+from .db_config import load_config, DB_URI, config_data
+from .db_connect import init_db, get_session, db
