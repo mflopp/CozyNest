2ce17ac071776b43a66f20da525fcafa9e8bcd79
Sam Malikin <workmalikin@gmail.com>
2024-12-06 10:58:58 +0200

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,9 +2,11 @@ from .logger import setup_logger
 from .env_utils import check_env_variable
 
 from .api_error import ValidationError
+from .api_error import get_details
 
 __all__ = [
     'ValidationError',
     'setup_logger',
-    'check_env_variable'
+    'check_env_variable',
+    'get_details'
 ]
