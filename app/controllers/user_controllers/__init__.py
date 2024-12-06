f34ca22144ebc1bffc0c5243b3d2d8d47a687b48
Sam Malikin <workmalikin@gmail.com>
2024-12-06 10:58:50 +0200

    Update __init__.py

--- app/controllers/user_controllers/__init__.py
+++ app/controllers/user_controllers/__init__.py
@@ -1,6 +1,13 @@
-from .user_controller_methods import *
-
+from .user_controller_methods import add_user
+from .user_controller_methods import del_user
+from .user_controller_methods import fetch_users
+from .user_controller_methods import fetch_user
+from .user_controller_methods import update_user_data
 
 __all__ = [
-    *user_controller_methods.__all__
+    'add_user',
+    'del_user',
+    'fetch_users',
+    'fetch_user',
+    'update_user_data',
 ]
