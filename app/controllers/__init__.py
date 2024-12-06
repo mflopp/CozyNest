545512b4676abd0ffd75bdb486a2ff3edb97ceab
Sam Malikin <workmalikin@gmail.com>
2024-12-06 10:58:31 +0200

    Update __init__.py

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,10 +1,21 @@
-from .user_controllers import *
+from .user_controllers import add_user, del_user
+from .user_controllers import update_user_data, fetch_user, fetch_users
+
 from .controller_utils import get_first_record_by_criteria
-from .controller_utils import validate_unique_field
+from .controller_utils import validate_unique_fields
+from .controller_utils import validate_user_required_fields
+from .controller_utils import validate_phone_number
 
 
 __all__ = [
-    *user_controllers.__all__,
+    'add_user',
+    'del_user',
+    'update_user_data',
+    'fetch_user',
+    'fetch_users',
+
     "get_first_record_by_criteria",
-    "validate_unique_field"
+    "validate_user_required_fields",
+    "validate_unique_fields",
+    "validate_phone_number"
 ]
