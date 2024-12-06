af1622c54448d693ef1b38c9665c7dcb0525b063
Sam Malikin <workmalikin@gmail.com>
2024-12-06 10:59:04 +0200

    Update api_error.py

--- app/utils/api_error.py
+++ app/utils/api_error.py
@@ -1,10 +1,44 @@
+from typing import Dict, Any
+
+
 class ValidationError(Exception):
-    """Custom exception for validation errors."""
-    def __init__(self, details: str):
+    """
+    Exception raised for validation errors.
+
+    Attributes:
+        title (str): The title of the error.
+        message (str): The detailed error message.
+        code (int): The error code.
+    """
+
+    def __init__(self, details: Dict[str, Any]):
         """
-        Initialize the exception with error details.
+        Initializes the ValidationError with error details.
 
         Args:
-            details (str): A description of the error.
+            details (Dict[str, Any]): A dictionary containing error details.
+                It must include the keys 'title', 'message', and 'code'.
+
+        Raises:
+            KeyError: If the required keys are missing in the details
+                      dictionary.
         """
-        self.details = details
+        if not all(key in details for key in ('title', 'message', 'code')):
+            msg = "Details must contain 'title', 'message', and 'code'."
+            raise KeyError(msg)
+
+        super().__init__(details.get('message', 'Validation error occurred.'))
+
+        self.title: str = details['title']
+        self.message: str = details['message']
+        self.code: int = details['code']
+
+
+def get_details(title: str, message: str, code: int):
+    details = {
+        'title': title,
+        'message': message,
+        'code': code
+    }
+
+    return details
