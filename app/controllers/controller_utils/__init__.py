3d5534c22afab4df0847672ac6edc4fb6b9f0899
Sam Malikin <workmalikin@gmail.com>
2024-12-06 10:58:37 +0200

    Update __init__.py

--- app/controllers/controller_utils/__init__.py
+++ app/controllers/controller_utils/__init__.py
@@ -1,10 +1,10 @@
-from .find_handler import get_first_record_by_criteria
+from .handlers import get_first_record_by_criteria
+from .handlers import fetch_record
 
-from .validations import validate_unique_field
-from .validations import validate_user_required_fields
+from .validations import validate_data
 
 __all__ = [
     'get_first_record_by_criteria',
-    'validate_unique_field',
-    'validate_user_required_fields'
+    'validate_data',
+    'fetch_record',
 ]
