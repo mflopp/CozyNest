8c3cbb550b042b0750e659515d47c1f4394a2d3f
Sam Malikin <workmalikin@gmail.com>
2024-12-07 03:22:29 +0200

    Created CRUDS for Country model

---
+++ app/controllers/accommodation_controllers/location_controller/country_controller/methods/__init__.py
@@ -0,0 +1,12 @@
+from .create_country import create_country
+from .delete_country import delete_country
+from .get_country import get_country
+from .get_countries import get_countries
+
+
+__all__ = [
+    'create_country',
+    'delete_country',
+    'get_country',
+    'get_countries'
+]
