511361ddf964897d5f457703c7aebb22c58aad3b
Sam Malikin <workmalikin@gmail.com>
2024-12-07 03:30:30 +0200

    Update get_user_settings.py

--- app/routes/user_settings_routes/get_user_settings.py
+++ app/routes/user_settings_routes/get_user_settings.py
@@ -6,8 +6,9 @@ from flask import make_response
 from collections import OrderedDict
 import json
 
+
 @user_settings_bp.route("", methods=['GET'])
-def get_user_settings_handler() -> tuple:
+def get_user_settings_handler():
     """
     Endpoint for retrieving all user settings from the database.
     Returns a list of user settings or an error message if not found.
@@ -23,14 +24,20 @@ def get_user_settings_handler() -> tuple:
 
         if user_settings:
             response_data = OrderedDict([("user settings", user_settings)])
-            response_json = json.dumps(response_data, default=str, sort_keys=False)
+            response_json = json.dumps(
+                response_data,
+                default=str,
+                sort_keys=False
+            )
             response = make_response(response_json, 200)
             response.headers['Content-Type'] = 'application/json'
+
             return response
 
         return "User settings not found", 404
     except Exception as e:
-        logging.error(f"Error occurred while retrieving user settings: {str(e)}")
+        logging.error(f"Error occurred while retrieving user settings:"
+                      f"{str(e)}")
         return "Error finding user settings", 500
     finally:
         if db:
