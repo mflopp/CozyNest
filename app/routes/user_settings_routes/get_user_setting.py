2817b4866d5dc94c70869c629b8544c18cea418b
Sam Malikin <workmalikin@gmail.com>
2024-12-07 03:30:22 +0200

    Update get_user_setting.py

--- app/routes/user_settings_routes/get_user_setting.py
+++ app/routes/user_settings_routes/get_user_setting.py
@@ -6,11 +6,13 @@ from .user_settings_blueprint import user_settings_bp
 from collections import OrderedDict
 import json
 
+
 @user_settings_bp.route("/getone", methods=['GET'])
-def get_user_setting_handler() -> tuple:
+def get_user_setting_handler():
     """
-    Endpoint for retrieving a user setting by ID. Looks up the user in the database
-    and returns the user setting data or a 404 error if not found.
+    Endpoint for retrieving a user setting by ID. Looks up the user
+    in the database and returns the user setting data or a 404 error
+    if not found.
 
     Args:
         id (int): The ID of the user setting to retrieve.
@@ -24,7 +26,7 @@ def get_user_setting_handler() -> tuple:
         db = next(get_session())  # Call get_session() to get a session
         user_setting = request.get_json()
         user_setting = fetch_user_setting(user_setting, db)
-        
+
         if user_setting:
             # Convert UserSettings object to OrderedDict
             user_setting_dict = OrderedDict([
@@ -32,15 +34,21 @@ def get_user_setting_handler() -> tuple:
                 ("currency", user_setting.currency),
                 ("language", user_setting.language)
             ])
-            
-            response_json = json.dumps(user_setting_dict, default=str, sort_keys=False)
+
+            response_json = json.dumps(
+                user_setting_dict,
+                default=str,
+                sort_keys=False
+            )
             response = make_response(response_json, 200)
             response.headers['Content-Type'] = 'application/json'
+
             return response
 
         return {"message": "User setting not found"}, 404
     except Exception as e:
-        logging.error(f"Error occurred while retrieving user setting: {str(e)}")
+        logging.error(f"Error occurred while retrieving user setting:"
+                      f"{str(e)}")
         return {"message": "Error finding user setting"}, 500
     finally:
         if db:
