2eef02e2d1e22f897fa6935f9782c081caa58772
Sam Malikin <workmalikin@gmail.com>
2024-12-07 03:30:34 +0200

    Update create_gender.py

--- app/controllers/user_controllers/gender_controller/create_gender.py
+++ app/controllers/user_controllers/gender_controller/create_gender.py
@@ -1,10 +1,6 @@
-import logging
 from sqlalchemy.orm import Session
-from sqlalchemy.sql import func
-
 from models.users import Gender
 
-from controllers.controller_utils import get_first_record_by_criteria
 
 from controllers.controller_utils.validations import validate_data
 
@@ -12,22 +8,20 @@ from general_controllers import add_record
 
 
 def add_gender(gender_data: dict, session: Session):
-    try:
-        fields = ['gender', 'description']
 
-        validate_data(
-            session=session,
-            Model=Gender,
-            data=gender_data,
-            required_fields=fields,
-            unique_fields=fields
-        )
+    fields = ['gender', 'description']
 
-        gender = Gender(
-            gender=gender_data['gender'],
-            description=gender_data['description']
-        )
+    validate_data(
+        session=session,
+        Model=Gender,
+        data=gender_data,
+        required_fields=fields,
+        unique_fields=fields
+    )
 
-        return add_record(session, gender, 'Gender')
-    except:
-        
+    gender = Gender(
+        gender=gender_data['gender'],
+        description=gender_data['description']
+    )
+
+    return add_record(session, gender, 'Gender')
