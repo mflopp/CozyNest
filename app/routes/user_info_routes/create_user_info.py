19700985faa573635e53ed827d066e3a5d2a90d2
Sam Malikin <workmalikin@gmail.com>
2024-12-07 17:45:59 +0200

    Update create_user_info.py

--- app/routes/user_info_routes/create_user_info.py
+++ app/routes/user_info_routes/create_user_info.py
@@ -9,24 +9,29 @@ from .user_info_blueprint import user_info_bp
 @user_info_bp.route("", methods=['POST'])
 def create_user_info_handler() -> tuple:
     """
-    Endpoint for creating a new user setting. Receives user data as JSON and returns
-    the result of creating a user setting in the database.
+    Endpoint for creating a new user setting. Receives user data as JSON
+    and returns the result of creating a user setting in the database.
 
     Returns:
         tuple: A tuple with the response and HTTP status code.
     """
-    db = None
+    session = None
 
     try:
-        db = next(get_session())
+        session = next(get_session())
 
         user_info = request.get_json()
-        response, status = add_user_info(user_info, db)
+        response, status = add_user_info(
+            user_data=user_info,
+            gender_id=user_info['gender_id'],
+            user_settings_id=user_info['user_settings_id'],
+            session=session
+        )
 
         return response, status
     except Exception as e:
         logging.error(f"Error occurred while creating user info: {str(e)}")
         return {"error": "Error creating user info"}, 500
     finally:
-        if db:
-            db.close()
+        if session:
+            session.close()
