c897a7546813ac788e82563820e0fcece6994d49
Sam Malikin <workmalikin@gmail.com>
2024-12-07 17:27:26 +0200

    Update get_all.py

--- app/routes/country_routes/get_all.py
+++ app/routes/country_routes/get_all.py
@@ -3,37 +3,74 @@ import logging
 from controllers import CountryController
 from config import get_session
 from .countries_blueprint import country_bp
-from flask import make_response
+from flask import make_response, Response
 from collections import OrderedDict
 import json
 
-@country_bp.route("", methods=['GET'])
-def get_countries_handler() -> tuple:
+from contextlib import contextmanager
+
+
+@contextmanager
+def session_scope():
     """
-    Endpoint for creating a new user setting. Receives user data as JSON and returns
-    the result of creating a user setting in the database.
+    Provides a transactional scope for the database session.
+
+    Yields:
+        session: The database session.
+    """
+    session = next(get_session())
+    try:
+        yield session
+    except Exception:
+        session.rollback()  # Rollback in case of an exception
+        raise
+    finally:
+        session.close()  # Always close the session
+
+
+def create_response(data, code) -> Response:
+    response_data = OrderedDict(data)
+    response_json = json.dumps(
+        response_data,
+        default=str,
+        sort_keys=False
+    )
+    response = make_response(response_json, code)
+    response.headers['Content-Type'] = 'application/json'
+
+    return response
+
+
+@country_bp.route("", methods=['GET'])
+def get_countries_handler() -> Response:
+    """
+    Handles the GET request to retrieve a list of countries.
 
     Returns:
-        tuple: A tuple with the response and HTTP status code.
+        Response: A JSON response containing the list of countries or
+                  an error message.
     """
-    session = None
-
     try:
-        session = next(get_session())
+        # Use the session_scope context manager
+        with session_scope() as session:
+            countries = CountryController.get_all(session)
 
-        countries = CountryController.get_all(session)
-        if countries:
-            response_data = OrderedDict([("countries", countries)])
-            response_json = json.dumps(response_data, default=str, sort_keys=False)
-            response = make_response(response_json, 200)
-            response.headers['Content-Type'] = 'application/json'
-            return response
-
-        return "Users not found", 404
+            if countries:
+                return create_response(
+                    data=[("countries", countries)],
+                    code=200
+                )
+            else:
+                return create_response(
+                    data=[("message", "Countries not found")],
+                    code=404
+                )
     except Exception as e:
-        logging.error(f"Error occurred while creating country data: {str(e)}")
-        return {"error": "Error creating country data"}, 500
-    finally:
-        if session:
-            session.close()
-            
+        logging.error(
+            f"Error occurred while retrieving country data: {str(e)}"
+        )
+
+        return create_response(
+            data=[("error", "Error retrieving country data")],
+            code=500
+        )
