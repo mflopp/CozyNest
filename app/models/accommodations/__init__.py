4448891750b725ee5d1961fada7d21cf43ec4a78
Sam Malikin <workmalikin@gmail.com>
2024-12-07 17:00:03 +0200

    Update __init__.py

--- app/models/accommodations/__init__.py
+++ app/models/accommodations/__init__.py
@@ -1,13 +1,33 @@
-from .accommodation_infos import *
-from .amenities import *
-from .rules import *
-from .sleeping_places import *
+from .accommodation_infos import AccommodationImage
+from .accommodation_infos import AccommodationType
+from .accommodation_infos import AccommodationAvailability
+
+from .amenities import AmenitiesCategory
+from .amenities import Amenity
+from .amenities import AccommodationAmenity
+
+from .rules import Rule
+from .rules import AccommodationRule
+
+from .sleeping_places import SleepingPlace
+from .sleeping_places import AccommodationSleepingPlace
+
 from .accommodation import Accommodation
 
 __all__ = [
-    *accommodation_infos.__all__,
-    *amenities.__all__,
-    *rules.__all__,
-    *sleeping_places.__all__,
+    'AccommodationImage',
+    'AccommodationType',
+    'AccommodationAvailability',
+
+    'AmenitiesCategory',
+    'Amenity',
+    'AccommodationAmenity',
+
+    'Rule',
+    'AccommodationRule',
+
+    'SleepingPlace',
+    'AccommodationSleepingPlace',
+
     "Accommodation"
 ]
