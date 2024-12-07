7c9336a8d6211dbae46a722ba1c2d5f5cc4c6e78
Sam Malikin <workmalikin@gmail.com>
2024-12-07 03:30:53 +0200

    Update __init__.py

--- app/controllers/general_controllers/__init__.py
+++ app/controllers/general_controllers/__init__.py
@@ -1,8 +1,10 @@
 from .create_record import add_record, save_record
-from .delete_record import del_record
+from .delete_record import del_record, delete_record
+
 
 __all__ = [
     'add_record',
     'save_record',
-    'del_record'
+    'del_record',
+    'delete_record'
 ]
