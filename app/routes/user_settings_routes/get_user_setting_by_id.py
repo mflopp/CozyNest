824f5607e21ff66a4c48a71913709f973822861a
Sam Malikin <workmalikin@gmail.com>
2024-12-07 03:30:27 +0200

    Update get_user_setting_by_id.py

--- app/routes/user_settings_routes/get_user_setting_by_id.py
+++ app/routes/user_settings_routes/get_user_setting_by_id.py
@@ -1,4 +1,4 @@
-from flask import make_response, jsonify
+from flask import make_response
 import logging
 from controllers.user_controllers import fetch_user_setting_by_id
 from config import get_session
@@ -6,11 +6,13 @@ from .user_settings_blueprint import user_settings_bp
 from collections import OrderedDict
 import json
 
+
 @user_settings_bp.route("<int:id>", methods=['GET'])
-def get_user_setting_by_id_handler(id: int) -> tuple:
+def get_user_setting_by_id_handler(id: int):
     """
-    Endpoint for retrieving a user setting by ID. Looks up the user in the database
-    and returns the user setting data or a 404 error if not found.
+    Endpoint for retrieving a user setting by ID. Looks up the user
+    in the database and returns the user setting data or a 404 error
+    if not found.
 
     Args:
         id (int): The ID of the user setting to retrieve.
@@ -31,15 +33,21 @@ def get_user_setting_by_id_handler(id: int) -> tuple:
                 ("currency", user_setting.currency),
                 ("language", user_setting.language)
             ])
-            
-            response_json = json.dumps(user_setting_dict, default=str, sort_keys=False)
+
+            response_json = json.dumps(
+                user_setting_dict,
+                default=str,
+                sort_keys=False
+            )
             response = make_response(response_json, 200)
             response.headers['Content-Type'] = 'application/json'
+
             return response
 
         return {"message": f"User setting ID {id} not found"}, 404
     except Exception as e:
-        logging.error(f"Error occurred while retrieving user setting {id}: {str(e)}")
+        logging.error(f"Error occurred while retrieving user setting {id}:"
+                      f"{str(e)}")
         return "Error finding user setting", 500
     finally:
         if db:
