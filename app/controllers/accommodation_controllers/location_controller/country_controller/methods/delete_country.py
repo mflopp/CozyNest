889b8af7944a1a729fd01560913dad56cbeeb980
Sam Malikin <workmalikin@gmail.com>
2024-12-08 15:41:05 +0200

    Update delete_country.py

--- app/controllers/accommodation_controllers/location_controller/country_controller/methods/delete_country.py
+++ app/controllers/accommodation_controllers/location_controller/country_controller/methods/delete_country.py
@@ -3,6 +3,7 @@ import logging
 from controllers.general_controllers import delete_record
 from .get_country import get_country
 
+
 def delete_country(id: int, session: Session):
     """
     Deletes a country by its ID from the database.
@@ -18,16 +19,29 @@ def delete_country(id: int, session: Session):
         404 Not Found: If the country is not found.
         500 Internal Server Error: If database operation fails.
     """
-    with session.begin_nested():
-        # Attempt to fetch the country
-        country = get_country(field='id', value=id, session=session)
-        # Attempt to delete the record
-        result = delete_record(
-            session=session,
-            record=country,
-            entity="Country"
-        )
+    try:
 
-    session.flush()
-    logging.info(f"Successfully deleted country with id={id}.")
-    return result
+        with session.begin_nested():
+            # Attempt to fetch the country
+            country = get_country(field='id', value=id, session=session)
+            # Attempt to delete the record
+            result = delete_record(
+                session=session,
+                record=country,
+                entity="Country"
+            )
+
+        session.flush()
+        logging.info(f"Successfully deleted country with id={id}.")
+        return result
+
+    except ValueError as ve:
+        logging.error(f"Deletion error: {ve}", exc_info=True)
+        raise
+
+    except Exception as e:
+        logging.error(
+            f"Unexpected error during deleting: {e}",
+            exc_info=True
+        )
+        raise
