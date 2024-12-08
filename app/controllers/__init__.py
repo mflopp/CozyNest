3e29b7bbed4e07e38a5dc238d69982e2693d4570
Sam Malikin <workmalikin@gmail.com>
2024-12-08 02:03:29 +0200

    Update __init__.py

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,9 +1,34 @@
 from .user_controllers import UserController
-from .user_controllers import add_user_setting, fetch_user_setting, fetch_user_settings, del_user_setting, fetch_user_setting_by_id, update_user_setting
-from .user_controllers import fetch_user_role, fetch_user_roles, fetch_user_role_by_id
-from .user_controllers import add_user_info, fetch_user_info, fetch_user_infos, del_user_info, update_user_info
-from .controller_utils import get_first_record_by_criteria, validate_unique_fields, validate_required_fields, validate_phone, validate_currency, validate_language
+
+from .user_controllers import add_user_setting
+from .user_controllers import fetch_user_setting
+from .user_controllers import fetch_user_settings
+from .user_controllers import del_user_setting
+from .user_controllers import fetch_user_setting_by_id
+from .user_controllers import update_user_setting
+
+from .user_controllers import fetch_user_role
+from .user_controllers import fetch_user_roles
+from .user_controllers import fetch_user_role_by_id
+
+from .user_controllers import add_user_info
+from .user_controllers import fetch_user_info
+from .user_controllers import fetch_user_infos
+from .user_controllers import del_user_info
+from .user_controllers import update_user_info
+
+from .controller_utils import get_first_record_by_criteria
+from .controller_utils import validate_unique_fields
+from .controller_utils import validate_required_fields
+from .controller_utils import validate_phone
+from .controller_utils import validate_currency
+from .controller_utils import validate_language
+
+from .controller_utils import fetch_record
+from .controller_utils import fetch_records
+
 from .accommodation_controllers import CountryController
+
 __all__ = [
     'UserController',
     'add_user',
@@ -27,10 +52,14 @@ __all__ = [
     'update_user_info',
     'validate_currency',
     'validate_language',
-    
+
     "get_first_record_by_criteria",
     "validate_required_fields",
     "validate_unique_fields",
     "validate_phone",
+
+    'fetch_record',
+    'fetch_records',
+
     'CountryController'
 ]
