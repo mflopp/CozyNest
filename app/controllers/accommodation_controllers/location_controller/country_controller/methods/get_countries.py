e2b760df7248b3e386fff7a37ede72fc705a3e69
Sam Malikin <workmalikin@gmail.com>
2024-12-08 02:20:17 +0200

    Update get_countries.py

--- app/controllers/accommodation_controllers/location_controller/country_controller/methods/get_countries.py
+++ app/controllers/accommodation_controllers/location_controller/country_controller/methods/get_countries.py
@@ -3,39 +3,24 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from typing import List
 
+from controllers.controller_utils import fetch_records
 from models.addresses import Country
-
-from ...utils import throw_error
 
 
 def get_countries(session: Session) -> List[Country]:
-    """
-    Retrieves a list of all countries from the database.
-
-    Args:
-        session (Session): SQLAlchemy session object.
-
-    Returns:
-        List[Country]: List of Country records.
-
-    Raises:
-        404 Not Found: If no countries are found in the database.
-        500 Internal Server Error: If database query fails.
-    """
     try:
         # Query all countries
-        countries = session.query(Country).all()
+        countries = fetch_records(session, Country, 'Country')
 
-        # Check if no countries are found
-        if not countries:
-            throw_error(404, "No countries found in the database.")
-
-        # Log number of countries found
-        logging.info(
-            f"{len(countries)} countries found in the database."
-        )
+        # Log the number of countries found
+        if countries:
+            logging.info(f"{len(countries)} countries found in the database.")
+        else:
+            logging.info("No countries found in the database.")
         return countries
-    except SQLAlchemyError as e:
-        throw_error(500, f"Database error while fetching countries: {e}")
-    except Exception as e:
-        throw_error(500, f"Unexpected error: {e}")
+    except SQLAlchemyError:
+        logging.error(
+            "Error while retrieving countries from the database.",
+            exc_info=True
+        )
+        raise
