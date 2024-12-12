8fe8d87fdb138ae9b6281228ced7155674049436
Alexander Kor <megaflopp@gmail.com>
2024-12-13 01:09:42 +0200

    Update validate_name.py

--- app/utils/validate_handler/methods/validate_name.py
+++ app/utils/validate_handler/methods/validate_name.py
@@ -17,11 +17,13 @@ CONDITIONS = [
     },
     {
         "pattern": r"^[A-Za-z]+(\s[A-Z][a-z]+)*$",
-        "error": "The name may contain only letters and spaces, but each word must start with an uppercase letter."
+        "error": "The name may contain only letters and spaces, but each word "
+        "must start with an uppercase letter."
     },
     {
         "pattern": r"^([A-Z][a-z]*\s){0,2}[A-Z][a-z]*$",
-        "error": "The name may contain at most three words separated by single spaces."
+        "error": "The name may contain at most three words separated by "
+        "single spaces."
     },
     {
         "pattern": r"^[A-Za-z\s]+$",
@@ -30,7 +32,7 @@ CONDITIONS = [
 ]
 
 
-def validate_geografic_name(name: str) -> None:
+def validate_name(name: str) -> None:
     logging.info('Validation of geografic name started!')
     try:
         if not name:
