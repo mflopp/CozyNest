d0fb2c23e658afa80f68dbda9a83040b5116b7d6
Sam Malikin <workmalikin@gmail.com>
2024-12-13 00:52:18 +0200

    Update validate_requirements.py

--- app/utils/validate_handler/methods/validate_requirements.py
+++ app/utils/validate_handler/methods/validate_requirements.py
@@ -1,12 +1,12 @@
-from typing import Callable
+from typing import Callable, List
 from utils.error_handler import ValidationError
 
 
 def validate_requirements(
-    field: str,
+    value: str,
     is_valid: Callable,
-    message: str
+    requirements: List[str]
 ) -> None:
-    if not is_valid(field):
-        err = f"{field} doesn't meet the required format: {message}"
+    if not is_valid(value):
+        err = f"{value} doesn't meet the required format: {requirements}"
         raise ValidationError(err)
