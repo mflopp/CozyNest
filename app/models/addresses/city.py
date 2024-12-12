e7a19d18cd1de5bf7aed23151054b268b2b8b464
Sam Malikin <workmalikin@gmail.com>
2024-12-13 00:44:46 +0200

    Update city.py

--- app/models/addresses/city.py
+++ app/models/addresses/city.py
@@ -1,34 +1,17 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from config import Base
 from sqlalchemy.orm import relationship
+from config import Base
 
 
 class City(Base):
-    """
-    Represents a city within a region.
-
-    Attributes:
-        id (int): The primary key of the city record.
-        region_id (int): A foreign key linking to the 'regions' table.
-        name (str): The name of the city.
-        region (Region): A relationship object representing
-                         the associated region.
-
-    Relationships:
-        region: Establishes a relationship with the `Region` model.
-
-    Example:
-        >>> city = City(name='Munich', region_id=1)
-        >>> print(city)
-        <City(id=1, name='Munich', region_id=1)>
-    """
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True)
     region_id = Column(Integer, ForeignKey('regions.id'), nullable=False)
     name = Column(String, nullable=False)
 
-    region = relationship("Region")
+    region = relationship("Country", back_populates='cities')
+    address = relationship("Address", back_populates='cities')
 
     def __repr__(self):
         return (f"<City(id={self.id}, name='{self.name}', "
