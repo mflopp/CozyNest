befb859a562e0ed8cbb26489447b1d8e066d9964
Sam Malikin <workmalikin@gmail.com>
2024-12-13 00:46:18 +0200

    Update add_record.py

--- app/utils/record_handler/methods/add_record.py
+++ app/utils/record_handler/methods/add_record.py
@@ -7,21 +7,14 @@ from .save_record import save_record
 
 
 def add_record(session: Session, record: Type[Any]):
-    """
-    Adds a record to the database session and commits the transaction.
-
-    Args:
-        session (Session): The database session.
-        record: The record to be added (must have an 'id' attribute).
-        entity (str): The name of the entity (e.g., 'User', 'Order').
-
-    Returns:
-        dict: A dictionary containing the status message and record ID
-        or error message.
-    """
     try:
+        if not record:
+            raise ValueError('There is no record to save')
+
         # Get entityname from Model arguments
         entity = record.__tablename__
+        if not entity:
+            raise ValueError('Record has no tablename')
 
         with session.begin_nested():
             logging.info('Starting saving data')
@@ -30,9 +23,7 @@ def add_record(session: Session, record: Type[Any]):
         message = f"Record successfully created in {entity}: {record.id}"
         logging.info(message)
 
-        # return {'message': message, 'id': record.id}, 200
     except SQLAlchemyError as e:
         error_message = f"Failed to create {record} in {entity}"
         logging.error(f'{error_message}: {str(e)}')
-
-        raise Exception(f"error: {error_message}, details: {str(e)}")
+        raise
