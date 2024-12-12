40d00576a24e5b66a5fa6abd4459ea96a6cb80f4
Sam Malikin <workmalikin@gmail.com>
2024-12-13 00:43:55 +0200

    Update accommodation_type.py

--- app/models/accommodations/accommodation_infos/accommodation_type.py
+++ app/models/accommodations/accommodation_infos/accommodation_type.py
@@ -1,39 +1,9 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from config import Base
 
 
 class AccommodationType(Base):
-    """
-    Represents different types of accommodation options available.
-
-    This class defines the schema for the 'accommodation_types' table,
-    which stores information about various accommodation categories,
-    such as 'Apartment', 'House', 'Villa', etc.
-
-    Attributes:
-        id (int): The primary key for the accommodation type.
-        accommodation_type (str): The type of accommodation,
-                                  defaulting to 'Apartment'.
-
-    Table:
-        Name: 'accommodation_types'
-        Columns:
-            - id: Primary key, Integer, Not Null
-            - accommodation_type: Name of the accommodation type, String,
-                                  Not Null, Unique
-
-    Example:
-        To create a new accommodation type:
-        >>> new_type = accommodationType(accommodation_type='Villa')
-        >>> session.add(new_type)
-        >>> session.commit()
-
-        To query accommodation types:
-        >>> accommodation_types = session.query(accommodationType).all()
-        >>> for ht in accommodation_types:
-        >>>     print(ht)
-        <accommodationType(id=1, accommodation_type='Apartment')>
-    """
     __tablename__ = 'accommodation_types'
 
     id = Column(Integer, primary_key=True)
@@ -42,6 +12,10 @@ class AccommodationType(Base):
         unique=True,
         nullable=False,
         default='Apartment'
+    )
+
+    accommodation = relationship(
+        "Accommodation", back_populates='accommodation_types'
     )
 
     def __repr__(self) -> str:
