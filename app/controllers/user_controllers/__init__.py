82f3d39bbc9de45c2cd226f6a69282b5dd1ce6a1
Alexander Kor <megaflopp@gmail.com>
2024-12-13 00:41:50 +0200

    Update __init__.py

--- app/controllers/user_controllers/__init__.py
+++ app/controllers/user_controllers/__init__.py
@@ -1,18 +1,13 @@
 from .user_controller import UserController
 from .user_role_controller import UserRoleController
+from .user_settings_controller import UserSettingsController
 
-from .user_settings_controller_methods import add_user_setting, fetch_user_setting, fetch_user_settings, del_user_setting, fetch_user_setting_by_id, update_user_setting
 from .user_info_controller_methods import add_user_info, fetch_user_info, fetch_user_infos, del_user_info, update_user_info
 
 __all__ = [
     'UserController',
     'UserRoleController',
-    'add_user_setting',
-    'fetch_user_setting',
-    'fetch_user_settings',
-    'del_user_setting',
-    'fetch_user_setting_by_id',
-    'update_user_setting',
+    'UserSettingsController',
     'add_user_info',
     'fetch_user_info',
     'fetch_user_infos',
