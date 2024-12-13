a2ea90d24102642bba937ae6af8b07d5f36e2bbc
Sam Malikin <workmalikin@gmail.com>
2024-12-14 00:19:14 +0200

    Update delete.py

--- app/routes/country_routes/delete.py
+++ app/routes/country_routes/delete.py
@@ -1,7 +1,9 @@
 import logging
 from flask import Response
+from sqlalchemy.exc import SQLAlchemyError
 
 from controllers import CountryController
+from utils.error_handler import NoRecordsFound, ValidationError, HasChildError
 from .countries_blueprint import country_bp
 from utils import create_response
 from config import session_scope
@@ -9,19 +11,6 @@ from config import session_scope
 
 @country_bp.route("/<int:id>", methods=['DELETE'])
 def delete_country_handler(id: int) -> Response:
-    """
-    Handle DELETE request to delete a country by its ID.
-
-    Args:
-        id (int): ID of the country to be deleted.
-
-    Returns:
-        Response: JSON response indicating the success or failure of
-                  the operation.
-
-    Raises:
-        Logs unexpected exceptions and returns a 500 error response.
-    """
     try:
         # Using session_scope context manager for database session
         with session_scope() as session:
@@ -33,16 +22,60 @@ def delete_country_handler(id: int) -> Response:
                 data=[("info", f'Deleted Succesfully {id}')],
                 code=200
             )
+
+    except ValueError as e:
+        logging.error(
+            f"Value Error occured while deleting: {str(e)}",
+            exc_info=True
+        )
+        # Returning validation error response
+        return create_response(
+            data=[("error", str(e))],
+            code=400
+        )
+
+    except SQLAlchemyError as e:
+        logging.error(
+            {f"Data Base error occurred while deleting: {e}"},
+            exc_info=True
+        )
+        return create_response(
+            data=[("error", str(e))],
+            code=400
+        )
+
+    except NoRecordsFound as e:
+        # Log unexpected errors with traceback
+        msg = f"No records found while deleting country with ID {id}"
+        logging.error(f"{msg}: {str(e)}", exc_info=True)
+        return create_response(
+            data=[("error", msg)],
+            code=404
+        )
+
+    except HasChildError as e:
+        # Log unexpected errors with traceback
+        msg = f"Error occurred while deleting country with ID {id}"
+        logging.error(f"{msg}: {str(e)}", exc_info=True)
+        return create_response(
+            data=[("error", msg)],
+            code=409
+        )
+
+    except ValidationError as e:
+        # Log unexpected errors with traceback
+        msg = f"Validation Error occurred while deleting country with ID {id}"
+        logging.error(f"{msg}: {str(e)}", exc_info=True)
+        return create_response(
+            data=[("error", msg)],
+            code=409
+        )
+
     except Exception as e:
         # Log unexpected errors with traceback
-        logging.error(
-            f"Error occurred while deleting country with ID {id}: {str(e)}",
-            exc_info=True
-            )
+        msg = "An unexpected error occurred while deleting"
+        logging.error(f"{msg}: {str(e)}", exc_info=True)
         return create_response(
-            data=[(
-                "error",
-                "An unexpected error occurred while deleting country data."
-            )],
+            data=[("error", msg)],
             code=500
         )
