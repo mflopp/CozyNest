8d05de238b9cc9f647217bc9f058f0df3a51af00
Sam Malikin <workmalikin@gmail.com>
2024-12-13 11:40:33 +0200

    Update validate_id.py

--- app/utils/validate_handler/methods/validate_id.py
+++ app/utils/validate_handler/methods/validate_id.py
@@ -1,29 +1,13 @@
+import logging
 from utils.error_handler import ValidationError
 
 
-def is_valid_id(id_value: int):
-    """
-    Validates if the given id is a positive integer.
-
-    Args:
-        id_value (int): The id to validate.
-
-    Returns:
-        bool: `True` if the id is a positive integer, otherwise `False`.
-    """
-    # Check if the id is an integer and positive
-    if not (isinstance(id_value, int) and id_value > 0):
-        raise ValidationError('ID must be positive integer')
-
-
 def validate_id(id_value: int) -> None:
-    """
-    Validates if the provided id is a positive integer.
+    logging.info('ID validation started')
 
-    Args:
-        id_value (int): The id to validate.
+    if not (isinstance(id_value, int) and id_value > 0):
+        msg = 'ID must be positive integer'
+        logging.info(msg)
+        raise ValidationError(msg)
 
-    Raises:
-        ValidationError: If the id is invalid.
-    """
-    is_valid_id(id_value)
+    logging.info('ID validation successfully finidhed')
