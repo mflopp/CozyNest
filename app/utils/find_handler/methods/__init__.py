c72bd76bbea6da61c859df724754f88b50fb61e1
Sam Malikin <workmalikin@gmail.com>
2024-12-13 10:07:59 +0200

    Update __init__.py

--- app/utils/find_handler/methods/__init__.py
+++ app/utils/find_handler/methods/__init__.py
@@ -1,19 +1,15 @@
-from .fetch_record import fetch_record
+from .fetch_record import fetch_one
 from .fetch_records import fetch_records
 from .filter_handler import set_filter_criteria
-from .get_first_record import get_first_record_by_criteria
 from .log_found_amount import log_found_amount
 from .get_full_record import get_full_record
-from .fetch_combination_record import fetch_combination_record
 from .fetch_relevant_values import fetch_relevant_values
 
 __all__ = [
-    'fetch_record',
+    'fetch_one',
     'fetch_records',
     'set_filter_criteria',
-    'get_first_record_by_criteria',
     'log_found_amount',
     'get_full_record',
-    'fetch_combination_record',
     'fetch_relevant_values'
 ]
