0791f23db530dc67a002a76cfe8c5c67673d43bf
Alexander Kor <megaflopp@gmail.com>
2024-12-14 00:27:00 +0200

    Update __init__.py

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,4 +1,7 @@
-from .user_controllers import UserController, UserRoleController, UserSettingsController
+from .user_controllers import (
+    UserController, UserRoleController, UserSettingsController,
+    GenderController
+)
 
 from .user_controllers import add_user_info
 from .user_controllers import fetch_user_info
@@ -6,26 +9,20 @@ from .user_controllers import fetch_user_infos
 from .user_controllers import del_user_info
 from .user_controllers import update_user_info
 
-from .accommodation_controllers import CountryController
-from .accommodation_controllers import RegionController
-from .accommodation_controllers import CityController
+from .accommodation_controllers import (
+    CountryController, RegionController, CityController
+)
 
 __all__ = [
     'UserController',
     'UserRoleController',
     'UserSettingsController',
+    'GenderController',
     'add_user_info',
     'fetch_user_info',
     'fetch_user_infos',
     'del_user_info',
     'update_user_info',
-
-    'Recorder',
-    'Finder',
-    'Validator',
-    'ValidationError',
-    'NoRecordsFound',
-
     'CountryController',
     'RegionController',
     'CityController'
