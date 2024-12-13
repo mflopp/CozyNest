42039397531d49c63aeef39ceeea570ac1ab6d3f
Alexander Kor <megaflopp@gmail.com>
2024-12-14 00:25:54 +0200

    Update __init__.py

--- app/utils/find_handler/methods/__init__.py
+++ app/utils/find_handler/methods/__init__.py
@@ -3,7 +3,7 @@ from .fetch_records import fetch_records
 from .filter_handler import set_filter_criteria
 from .log_found_amount import log_found_amount
 from .get_full_record import get_full_record
-from .fetch_relevant_values import fetch_relevant_values
+from .extract_required_data import extract_required_data
 
 __all__ = [
     'fetch_one',
@@ -11,5 +11,5 @@ __all__ = [
     'set_filter_criteria',
     'log_found_amount',
     'get_full_record',
-    'fetch_relevant_values'
+    'extract_required_data'
 ]
