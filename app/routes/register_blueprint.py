510e758f224e0d429d0ee93a6a52ed3636ab2299
Alexander Kor <megaflopp@gmail.com>
2024-12-14 01:58:43 +0200

    user roles bp added

--- app/routes/register_blueprint.py
+++ app/routes/register_blueprint.py
@@ -1,9 +1,11 @@
 from .country_routes import country_bp
 from .user_settings_routes import user_settings_bp
 from .gender_routes import genders_bp
+from .user_roles_routes import user_roles_bp
 
 
 def register_all_blueprints(app):
     app.register_blueprint(country_bp)
     app.register_blueprint(user_settings_bp)
     app.register_blueprint(genders_bp)
+    app.register_blueprint(user_roles_bp)
