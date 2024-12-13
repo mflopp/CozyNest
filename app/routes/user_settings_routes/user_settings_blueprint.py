1d38b6c36b42f512c420a2fd6d0ae80cdabc2ba5
Alexander Kor <megaflopp@gmail.com>
2024-12-13 12:24:44 +0200

    Update user_settings_blueprint.py

--- app/routes/user_settings_routes/user_settings_blueprint.py
+++ app/routes/user_settings_routes/user_settings_blueprint.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
 
 # Create a blueprint for users CRUD
-user_settings_bp = Blueprint('user_settings', __name__, url_prefix="/users/settings")
+user_settings_bp = Blueprint(
+    'user_settings', __name__, url_prefix="/users/settings"
+)
