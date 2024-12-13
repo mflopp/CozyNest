5175037cfc3279178c5abeee571c740523440aa7
Alexander Kor <megaflopp@gmail.com>
2024-12-14 00:27:07 +0200

    Update __init__.py

--- app/controllers/user_controllers/__init__.py
+++ app/controllers/user_controllers/__init__.py
@@ -1,13 +1,18 @@
 from .user_controller import UserController
 from .user_role_controller import UserRoleController
 from .user_settings_controller import UserSettingsController
+from .gender_controller import GenderController
 
-from .user_info_controller_methods import add_user_info, fetch_user_info, fetch_user_infos, del_user_info, update_user_info
+from .user_info_controller_methods import (
+    add_user_info, fetch_user_info, fetch_user_infos,
+    del_user_info, update_user_info
+)
 
 __all__ = [
     'UserController',
     'UserRoleController',
     'UserSettingsController',
+    'GenderController',
     'add_user_info',
     'fetch_user_info',
     'fetch_user_infos',
