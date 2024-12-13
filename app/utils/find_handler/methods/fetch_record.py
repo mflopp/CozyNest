8fde3bc298b83a499c3eb73ce1a4713abaa7f8a2
Sam Malikin <workmalikin@gmail.com>
2024-12-13 10:08:06 +0200

    Update fetch_record.py

--- app/utils/find_handler/methods/fetch_record.py
+++ app/utils/find_handler/methods/fetch_record.py
@@ -1,31 +1,23 @@
 import logging
 from sqlalchemy.orm import Session
-from typing import Any, Type
+from typing import Any, Type, Dict
 
 from sqlalchemy.exc import SQLAlchemyError
-from .filter_handler import set_filter_criteria
-from .get_first_record import get_first_record_by_criteria
 
 
-def fetch_record(session: Session, Model: Type[Any],
-                 field: str, value: Any) -> Any:
+def fetch_one(
+    session: Session,
+    Model: Type[Any],
+    criteria: Dict[str, Any]
+) -> Any:
     try:
         # Get the name of the model for logging purposes
         model_name = Model.__name__
 
-        # Set dynamic filter criteria based on the given field and value
-        criteria = set_filter_criteria(field, value)
-        if criteria:
-            logging.warning(f'Criteria: {criteria}')
+        record = session.query(Model).filter_by(**criteria).first()
 
-        # Fetch the first matching record
-        record = get_first_record_by_criteria(session, Model, criteria)
-
-        # Log if no record is found
         if not record:
-            logging.info(
-                f"No {model_name} record found where '{field}'='{value}'"
-            )
+            logging.warning(f'No {model_name} record found where: {criteria}')
             return None
 
         # Log the ID of the fetched record if it exists
@@ -34,9 +26,7 @@ def fetch_record(session: Session, Model: Type[Any],
 
     except SQLAlchemyError as e:
         # Improved logging with detailed database context
-        data = f"{model_name} with '{field}'='{value}': {e}"
-        logging.error(
-            f"Error occurred while fetching {data}"
-        )
+        data = f"{model_name} with '{criteria}': {e}"
+        msg = f"Error occurred while fetching: {data}."
         # Raise exception to handle upstream
-        raise
+        raise SQLAlchemyError(msg)
