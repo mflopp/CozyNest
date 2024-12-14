2f2afb8b3471079ec93d8e255fc7123f8c78ae89
Alexander Kor <megaflopp@gmail.com>
2024-12-14 12:46:55 +0200

    Update get_user_info.py

--- app/routes/user_info_routes/get_user_info.py
+++ app/routes/user_info_routes/get_user_info.py
@@ -1,52 +1,56 @@
-from flask import make_response
 import logging
-from controllers.user_controllers import fetch_user_info
-from config import get_session
+from sqlalchemy.exc import SQLAlchemyError
+
+from controllers import UserInfoController
 from .user_info_blueprint import user_info_bp
-from collections import OrderedDict
-import json
+
+from utils import create_response
+from config import session_scope
+
 
 @user_info_bp.route("<int:id>", methods=['GET'])
-def get_user_info_handler(id: int) -> tuple:
+def get_user_info_handler(id: int):
     """
-    Endpoint for retrieving a user setting by ID. Looks up the user in the database
-    and returns the user setting data or a 404 error if not found.
+    Endpoint for retrieving a user info by ID. Looks up the user
+    in the database and returns the user info data or a 404 error if not found.
 
     Args:
-        id (int): The ID of the user setting to retrieve.
+        id (int): The ID of the user info to retrieve.
 
     Returns:
         tuple: A tuple with the response and HTTP status code.
     """
-    db = None
-
     try:
-        db = next(get_session())  # Call get_session() to get a session
-        user_info = fetch_user_info(id, db)
+        # Use the session_scope context manager
+        with session_scope() as session:
+            user_info = UserInfoController.get_one(id, session)
 
-        if user_info:
-            # Convert UserSettings object to OrderedDict
-            user_info_dict = OrderedDict([
-                ("id", user_info.id),
-                ("gender_id", user_info.gender_id),
-                ("user_settings_id", user_info.user_settings_id),
-                ("first_name", user_info.first_name),
-                ("last_name", user_info.last_name),
-                ("birthdate", user_info.birthdate),
-                ("created_at", user_info.created_at),
-                ("updated_at", user_info.updated_at)
-            ])
+            if user_info:
+                return create_response(
+                    data=[
+                        ("id", user_info.id),
+                        ("gender_id", user_info.gender_id),
+                        ("user_settings_id", user_info.user_settings_id),
+                        ("first_name", user_info.first_name),
+                        ("last_name", user_info.last_name),
+                        ("birthdate", user_info.birthdate),
+                        ("created_at", user_info.created_at),
+                        ("updated_at", user_info.updated_at)
+                    ],
+                    code=200
+                )
 
-    
-            response_json = json.dumps(user_info_dict, default=str, sort_keys=False)
-            response = make_response(response_json, 200)
-            response.headers['Content-Type'] = 'application/json'
-            return response
+            return {"message": f"User setting ID {id} not found"}, 404
 
-        return {"message": f"User setting ID {id} not found"}, 404
+    except SQLAlchemyError as e:
+        msg = f"DB error occured while fetching user info ID  {id}: {str(e)}"
+        logging.error(msg)
+        return create_response(
+            data=[("error", msg)],
+            code=500
+        )
+
     except Exception as e:
-        logging.error(f"Error occurred while retrieving user setting {id}: {str(e)}")
-        return "Error finding user setting", 500
-    finally:
-        if db:
-            db.close()  # Ensure the database session is properly closed
+        msg = f"Unexpected error while getting user info ID  {id}: {e}"
+        logging.error(msg)
+        return create_response(data=[("error", msg)], code=500)
