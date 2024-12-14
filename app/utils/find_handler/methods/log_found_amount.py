bfffaa9191abd4d703eadd8e2353072a4397436d
Sam Malikin <workmalikin@gmail.com>
2024-12-14 07:15:51 +0200

    Update log_found_amount.py

--- app/utils/find_handler/methods/log_found_amount.py
+++ app/utils/find_handler/methods/log_found_amount.py
@@ -4,15 +4,6 @@ from utils.error_handler import NoRecordsFound
 
 
 def log_found_amount(records: List[Any]) -> None:
-    """
-    Logs the number of records found in the database.
-
-    Args:
-        records (List[Any]): A list of records retrieved from the database.
-
-    Raises:
-        NoRecordsFound: If no records are found in the list.
-    """
     records_count = len(records)
 
     if records_count:
