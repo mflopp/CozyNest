2d1e08831e64510d32ad3f239dd3ca18f6061fd4
Alexander Kor <megaflopp@gmail.com>
2024-12-14 12:45:55 +0200

    Create user_info_controller.py

---
+++ app/controllers/user_controllers/user_info_controller/user_info_controller.py
@@ -0,0 +1,43 @@
+from typing import List, Dict
+from sqlalchemy.orm import Session
+
+from models import UserInfo
+from .methods import (
+    add_user_info, fetch_user_info, fetch_user_infos,
+    del_user_info, update_user_info
+)
+
+
+class UserInfoController:
+    @staticmethod
+    def create(data: Dict, session: Session) -> UserInfo:
+        user_info = add_user_info(data, session)
+        session.commit()
+        return user_info
+
+    # @staticmethod
+    # def get_one_by_name(data: Dict, session: Session) -> Userinfos:
+    #     user_info = fetch_user_info(data, session)
+    #     return user_info
+
+    @staticmethod
+    def get_one(user_info_id: int, session: Session) -> UserInfo:
+        user_info = fetch_user_info(user_info_id, session)
+        return user_info
+
+    @staticmethod
+    def get_all(session: Session) -> List:
+        user_infos = fetch_user_infos(session)
+        return user_infos
+
+    @staticmethod
+    def delete(user_id: int, session: Session) -> List:
+        result = del_user_info(user_id, session)
+        session.commit()
+        return result
+
+    @staticmethod
+    def update(user_id: int, data: Dict, session: Session) -> List:
+        result = update_user_info(user_id, data, session)
+        session.commit()
+        return result
