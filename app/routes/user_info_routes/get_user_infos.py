48f594cd6f5813691077286152d6185242f3761e
Alexander Kor <megaflopp@gmail.com>
2024-12-14 12:46:58 +0200

    Update get_user_infos.py

--- app/routes/user_info_routes/get_user_infos.py
+++ app/routes/user_info_routes/get_user_infos.py
@@ -1,12 +1,15 @@
 import logging
-from controllers.user_controllers import fetch_user_infos
+from sqlalchemy.exc import SQLAlchemyError
+
+from controllers import UserInfoController
 from .user_info_blueprint import user_info_bp
 
 from utils import create_response
 from config import session_scope
 
+
 @user_info_bp.route("", methods=['GET'])
-def get_user_infos_handler() -> tuple:
+def get_user_infos_handler():
     """
     Endpoint for retrieving all user settings from the database.
     Returns a list of user settings or an error message if not found.
@@ -17,20 +20,28 @@ def get_user_infos_handler() -> tuple:
     try:
         # Use the session_scope context manager
         with session_scope() as session:
-            user_infos = fetch_user_infos(session)
+            user_infos = UserInfoController.get_all(session)
 
             if user_infos:
-                response_data = OrderedDict([("user infos", user_infos)])
-                response_json = json.dumps(response_data, default=str, sort_keys=False)
-                response = make_response(response_json, 200)
-                response.headers['Content-Type'] = 'application/json'
-                return response
+                return create_response(
+                    data=[("user infos", user_infos)],
+                    code=200
+                )
 
-            return "User infos not found", 404
+            return {"message": "Gender not found"}, 404
+
+    except SQLAlchemyError as e:
+        msg = f"DB error occured while fetching user infos: {str(e)}"
+        logging.error(msg)
+        return create_response(
+            data=[("error", msg)],
+            code=500
+        )
 
     except Exception as e:
-        logging.error(f"Error occurred while retrieving user infos: {str(e)}")
+        msg = f"Error occurred while fetching user infos: {str(e)}"
+        logging.error(msg)
         return create_response(
-            data=[("error", f"Error finding user info: {str(e)}")],
+            data=[("error", msg)],
             code=500
         )
