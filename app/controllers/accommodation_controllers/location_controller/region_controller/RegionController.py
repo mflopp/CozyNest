90d8f72aa6c90cc961f5c7c0cf4ee95b44ae2026
Sam Malikin <workmalikin@gmail.com>
2024-12-14 07:15:45 +0200

    Create RegionController.py

---
+++ app/controllers/accommodation_controllers/location_controller/region_controller/RegionController.py
@@ -0,0 +1,41 @@
+from typing import List, Dict
+from sqlalchemy.orm import Session
+from models import Region
+from .methods import (
+    create_region,
+    get_region,
+    get_regions,
+    delete_region,
+    update_region
+)
+
+
+class RegionController:
+    @staticmethod
+    def create(data: dict, session: Session) -> Region:
+        result = create_region(data, session)
+        session.commit()
+        return result
+
+    @staticmethod
+    def get_region(region_id: int, session: Session) -> Region:
+        result = get_region(region_id, session)
+        session.commit()
+        return result
+
+    @staticmethod
+    def get_all(session: Session) -> List:
+        result = get_regions(session)
+        session.commit()
+        return result
+
+    @staticmethod
+    def delete(region_id: int, session: Session):
+        delete_region(region_id, session)
+        session.commit()
+
+    @staticmethod
+    def update(region_id: int, data: Dict, session: Session) -> Region:
+        result = update_region(region_id, data, session)
+        session.commit()
+        return result
