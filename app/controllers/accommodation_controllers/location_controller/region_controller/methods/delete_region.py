2027c813628fd0e3585978b3dfbffa92b3c46fb0
Sam Malikin <workmalikin@gmail.com>
2024-12-14 07:15:32 +0200

    Update delete_region.py

--- app/controllers/accommodation_controllers/location_controller/region_controller/methods/delete_region.py
+++ app/controllers/accommodation_controllers/location_controller/region_controller/methods/delete_region.py
@@ -2,38 +2,59 @@ import logging
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 
-from utils import Recorder
-from utils.error_handler import ValidationError
+from models import City
+from utils import Recorder, Validator
+from utils.error_handler import (
+    ValidationError,
+    NoRecordsFound,
+    HasChildError
+)
 
 from .get_region import get_region
+
+ERR_MSG = "Error occurred while deleting"
+TRACEBACK = True
 
 
 def delete_region(id: int, session: Session):
     try:
-        with session.begin_nested():
-            # Attempt to fetch the region
-            region = get_region(field='id', value=id, session=session)
+        Validator.validate_id(id)
 
-            # Attempt to delete the record
+        with session.begin_nested():
+            region = get_region(id, session)
+            if not region:
+                logging.info('No Region to delete')
+                raise NoRecordsFound
+
+            if Recorder.has_child(region, City):
+                raise HasChildError
+
             Recorder.delete(session, region)
+
+    except HasChildError as e:
+        logging.error(e, exc_info=TRACEBACK)
+        raise
+
+    except NoRecordsFound as e:
+        logging.error(
+            f"No records found for deleting: {e}", exc_info=TRACEBACK
+        )
+        raise
 
     except ValidationError as e:
         logging.error(
-            {f"Validation error occurred while deleting: {e}"},
-            exc_info=True
+            f"Validation {ERR_MSG}: {e}", exc_info=TRACEBACK
         )
         raise
 
     except SQLAlchemyError as e:
         logging.error(
-            {f"Data Base error occurred while deleting: {e}"},
-            exc_info=True
+            f"Data Base {ERR_MSG}: {e}", exc_info=TRACEBACK
         )
         raise
 
     except Exception as e:
         logging.error(
-            {f"Unexpected error while deleting: {e}"},
-            exc_info=True
+            f"Unexpected {ERR_MSG}: {e}", exc_info=TRACEBACK
         )
         raise
