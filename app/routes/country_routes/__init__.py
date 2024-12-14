6a4bb8a5025cd03639be53dd39f63408474501d7
Sam Malikin <workmalikin@gmail.com>
2024-12-14 02:44:58 +0200

    Update __init__.py

--- app/routes/country_routes/__init__.py
+++ app/routes/country_routes/__init__.py
@@ -1,8 +1,7 @@
 from .countries_blueprint import country_bp
 from .create import create_country_handler
 from .get_all import get_countries_handler
-from .get_by_name import get_country_by_name_handler
-from .get_by_id import get_country_by_id_handler
+from .get_by_id import get_country_handler
 from .delete import delete_country_handler
 from .update import update_country_handler
 
@@ -11,9 +10,7 @@ __all__ = [
     'country_bp',
     'create_country_handler',
     'get_countries_handler',
-    'get_country_by_name_handler',
-    'get_country_by_id_handler',
+    'get_country_handler',
     'delete_country_handler',
     'update_country_handler'
-
 ]
