4615c3ac2e82927cb325aa34b6c76ead2acdf3c8
Sam Malikin <workmalikin@gmail.com>
2024-12-14 07:15:34 +0200

    Update get_region.py

--- app/controllers/accommodation_controllers/location_controller/region_controller/methods/get_region.py
+++ app/controllers/accommodation_controllers/location_controller/region_controller/methods/get_region.py
@@ -1,53 +1,48 @@
 import logging
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
-from typing import Any
 
 from models import Region
-from utils import Validator, Finder
-from utils.error_handler import ValidationError
+from utils import Finder, Validator
+from utils.error_handler import ValidationError, NoRecordsFound
+
+ERR_MSG = "Error occurred while fetching Region record"
 
 
-def get_region(field: str, value: Any, session: Session) -> Region:
+def get_region(id: int, session: Session) -> Region:
     try:
-        possible_fields = {'id', 'name'}
-
-        if field not in possible_fields:
-            raise ValidationError(f'Field must be in {possible_fields}')
-
-        if field == "id":
-            Validator.validate_id(value)
-
-        if field == "name":
-            Validator.validate_name(value)
+        Validator.validate_id(id)
 
         # Retrieve the record
         region = Finder.fetch_record(
             session=session,
             Model=Region,
-            field=field,
-            value=value
+            criteria={'id': id}
         )
 
-        return region
+        if region:
+            return region
+
+        raise NoRecordsFound
+
+    except NoRecordsFound as e:
+        logging.error(e, exc_info=True)
+        raise
 
     except ValidationError as e:
         logging.error(
-            {f"Validation Error occurred while fetching record: {e}"},
-            exc_info=True
+            f"Validation {ERR_MSG}: {e}", exc_info=True
         )
         raise
 
     except SQLAlchemyError as e:
         logging.error(
-            {f"Data Base Error occurred while fetching record: {e}"},
-            exc_info=True
+            f"Data Base {ERR_MSG}: {e}", exc_info=True
         )
         raise
 
     except Exception as e:
         logging.error(
-            {f"Unexpected Error occurred while fetching record: {e}"},
-            exc_info=True
+            f"Unexpected {ERR_MSG}: {e}", exc_info=True
         )
         raise
