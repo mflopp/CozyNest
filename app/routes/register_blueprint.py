cdad8883e84a7954479d1537b7693969f7089bbd
Alexander Kor <megaflopp@gmail.com>
2024-12-14 12:46:47 +0200

    Update register_blueprint.py

--- app/routes/register_blueprint.py
+++ app/routes/register_blueprint.py
@@ -2,6 +2,7 @@ from .country_routes import country_bp
 from .user_settings_routes import user_settings_bp
 from .gender_routes import genders_bp
 from .user_roles_routes import user_roles_bp
+from .user_info_routes import user_info_bp
 
 
 def register_all_blueprints(app):
@@ -9,3 +10,4 @@ def register_all_blueprints(app):
     app.register_blueprint(user_settings_bp)
     app.register_blueprint(genders_bp)
     app.register_blueprint(user_roles_bp)
+    app.register_blueprint(user_info_bp)
