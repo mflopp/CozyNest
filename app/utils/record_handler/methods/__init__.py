24a40053ef7aae243a136694298c89ec06a6d4cd
Sam Malikin <workmalikin@gmail.com>
2024-12-17 20:03:52 +0200

    Update __init__.py

--- app/utils/record_handler/methods/__init__.py
+++ app/utils/record_handler/methods/__init__.py
@@ -3,11 +3,13 @@ from .add_record import add_record
 from .update_record import update_record
 from .delete_record import delete_record
 from .has_child import has_child
+from .parse_record import parse_record
 
 __all__ = [
     'save_record',
     'add_record',
     'update_record',
     'delete_record',
-    'has_child'
+    'has_child',
+    'parse_record'
 ]
