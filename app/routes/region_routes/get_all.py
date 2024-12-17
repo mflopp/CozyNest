058ac4f69d6b13476be2639d0e48645836634ec3
Sam Malikin <workmalikin@gmail.com>
2024-12-17 20:03:40 +0200

    Update get_all.py

--- app/routes/region_routes/get_all.py
+++ app/routes/region_routes/get_all.py
@@ -2,75 +2,43 @@ import logging
 from flask import Response
 from sqlalchemy.exc import SQLAlchemyError
 
-from controllers import CountryController
+from controllers import RegionController
 from utils.error_handler import NoRecordsFound, ValidationError
 from utils import create_response
 from config import session_scope
 from .regions_blueprint import region_bp
 
+ERR_MSG = "Error occurred while fetching Region records"
+TRACEBACK = True
 
-@country_bp.route("", methods=['GET'])
-def get_countries_handler() -> Response:
+
+@region_bp.route("", methods=['GET'])
+def get_regions_handler() -> Response:
     try:
         with session_scope() as session:
-            countries = CountryController.get_all(session)
-
-            if countries:
-                return create_response(
-                    data=[("countries", countries)],
-                    code=200
-                )
+            regions = RegionController.get_all(session)
 
             return create_response(
-                data=[("message", "Countries not found")],
-                code=404
+                data=[("regions", regions)],
+                code=200
             )
 
-    except ValidationError as e:
-        msg = "Validation Error occurred while fetching countries"
-        logging.error(f"{msg}: {str(e)}", exc_info=True)
-
-        return create_response(
-            data=[("error", msg)],
-            code=409
-        )
-
-    except NoRecordsFound as e:
-        msg = "No records found while fetching countries"
-        logging.error(f"{msg}: {str(e)}", exc_info=True)
-
-        return create_response(
-            data=[("error", msg)],
-            code=404
-        )
-
     except ValueError as e:
-        logging.error(
-            f"Value Error occured while fetching: {str(e)}",
-            exc_info=True
-        )
-
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+        logging.error(f"Value {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
+        return create_response(data=[("error", str(e))], code=400)
 
     except SQLAlchemyError as e:
-        logging.error(
-            {f"Data Base error occurred while fetching: {e}"},
-            exc_info=True
-        )
+        logging.error(f"Data Base {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
+        return create_response(data=[("error", str(e))], code=400)
 
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+    except NoRecordsFound as e:
+        logging.error(f"NoRecordsFound {ERR_MSG}:{str(e)}", exc_info=TRACEBACK)
+        return create_response(data=[("error", str(e))], code=404)
+
+    except ValidationError as e:
+        logging.error(f"Validation {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
+        return create_response(data=[("error", str(e))], code=409)
 
     except Exception as e:
-        msg = "An unexpected error occurred while fetching"
-        logging.error(f"{msg}: {str(e)}", exc_info=True)
-
-        return create_response(
-            data=[("error", msg)],
-            code=500
-        )
+        logging.error(f"Unexpected {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
+        return create_response(data=[("error", ERR_MSG)], code=500)
