8365de0df864ca24241fffbaee33ab0452152dea
Sam Malikin <workmalikin@gmail.com>
2024-12-17 20:03:50 +0200

    Update register_blueprint.py

--- app/routes/register_blueprint.py
+++ app/routes/register_blueprint.py
@@ -1,4 +1,6 @@
 from .country_routes import country_bp
+from .region_routes import region_bp
+
 from .user_settings_routes import user_settings_bp
 from .gender_routes import genders_bp
 from .user_roles_routes import user_roles_bp
@@ -8,6 +10,8 @@ from .user_routes import users_bp
 
 def register_all_blueprints(app):
     app.register_blueprint(country_bp)
+    app.register_blueprint(region_bp)
+
     app.register_blueprint(user_settings_bp)
     app.register_blueprint(genders_bp)
     app.register_blueprint(user_roles_bp)
