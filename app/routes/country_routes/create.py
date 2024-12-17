55dbc74d0ab8c1fa4522135e1c7ec7d732836ca4
Sam Malikin <workmalikin@gmail.com>
2024-12-17 20:03:33 +0200

    Update create.py

--- app/routes/country_routes/create.py
+++ app/routes/country_routes/create.py
@@ -9,6 +9,9 @@ from .countries_blueprint import country_bp
 from utils import create_response
 from config import session_scope
 
+ERR_MSG = 'Error occurred while creating Country record'
+TRACEBACK = True
+
 
 @country_bp.route("", methods=['POST'])
 def create_country_handler() -> Response:
@@ -20,59 +23,42 @@ def create_country_handler() -> Response:
                 code=400
             )
 
-        # Using session_scope context manager for database session
         with session_scope() as session:
-            # Creating the country via the controller
             country = CountryController.create(request_data, session)
             if country:
-                # Returning success response with created country data
                 return create_response(
                     data=[("country", country)],
                     code=200
                 )
 
-            # Returning response for unsuccessful creation
             return create_response(
                 data=[("message", "Country not created")],
                 code=400
             )
 
     except ValidationError as e:
-        # Logging validation error
-        logging.error(f"Error occurred while creating country: {str(e)}")
-
-        # Returning validation error response
+        logging.error(f"Validation {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
         return create_response(
             data=[("error", str(e))],
             code=400
         )
 
     except ValueError as e:
-        logging.error(
-            f"Value Error occured while creating: {str(e)}",
-            exc_info=True
-        )
-        # Returning validation error response
+        logging.error(f"Value {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
         return create_response(
             data=[("error", str(e))],
             code=400
         )
 
     except SQLAlchemyError as e:
-        logging.error(
-            {f"Data Base error occurred while creating: {e}"},
-            exc_info=True
-        )
+        logging.error({f"Data Base {ERR_MSG}: {e}"}, exc_info=TRACEBACK)
         return create_response(
             data=[("error", str(e))],
             code=400
         )
 
     except Exception as e:
-        # Logging unexpected error with traceback
-        logging.error(f"Error occurred while creating country data: {str(e)}")
-
-        # Returning general error response
+        logging.error(f"Unexpected {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
         return create_response(
             data=[("error", "Error creating country data")],
             code=500
