37603dbb767b77ad4a0ffa2215ac1525b7aa1120
Sam Malikin <workmalikin@gmail.com>
2024-12-18 20:40:05 +0200

    Update delete.py

--- app/routes/region_routes/delete.py
+++ app/routes/region_routes/delete.py
@@ -1,4 +1,3 @@
-import logging
 from flask import Response
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -6,10 +5,10 @@ from controllers import RegionController
 from utils.error_handler import NoRecordsFound, ValidationError, HasChildError
 from utils import create_response
 from config import session_scope
+from utils.logs_handler import log_err
 from .regions_blueprint import region_bp
 
 ERR_MSG = 'Error occurred while deleting Region record'
-TRACEBACK = True
 
 
 @region_bp.route("/<int:id>", methods=['DELETE'])
@@ -24,26 +23,25 @@ def delete_region_handler(id: int) -> Response:
             )
 
     except ValueError as e:
-        logging.error(f"Value {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
-        return create_response(data=[("error", str(e))], code=400)
-
-    except SQLAlchemyError as e:
-        logging.error(f"Data Base {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
+        log_err(f"Value {ERR_MSG}: {str(e)}")
         return create_response(data=[("error", str(e))], code=400)
 
     except NoRecordsFound as e:
-        logging.error(f"NoRecordsFound {ERR_MSG}:{str(e)}", exc_info=TRACEBACK)
+        log_err(f"{ERR_MSG}: {str(e)}")
         return create_response(data=[("error", str(e))], code=404)
 
-    except ValidationError as e:
-        logging.error(f"Validation {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
+    except HasChildError as e:
+        log_err(f"{ERR_MSG}: {str(e)}")
         return create_response(data=[("error", str(e))], code=409)
 
-    except HasChildError as e:
-        msg = f"{ERR_MSG} with ID {id}"
-        logging.error(f"{msg}: {str(e)}", exc_info=TRACEBACK)
-        return create_response(data=[("error", msg)], code=409)
+    except ValidationError as e:
+        log_err(f"Validation {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=409)
+
+    except SQLAlchemyError as e:
+        log_err(f"Data Base {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=500)
 
     except Exception as e:
-        logging.error(f"Unexpected {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
-        return create_response(data=[("error", ERR_MSG)], code=500)
+        log_err(f"Unexpected {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", "Deletion error")], code=500)
