22c1e5a875ad34dee58cf8136e8b52a9b20f00eb
Sam Malikin <workmalikin@gmail.com>
2024-12-18 02:35:22 +0200

    Update NoRecordsFound.py

--- app/utils/error_handler/NoRecordsFound.py
+++ app/utils/error_handler/NoRecordsFound.py
@@ -1,4 +1,4 @@
 class NoRecordsFound(Exception):
     """Exception for case when there are no records in the request."""
-    def __init__(self, message="Not found", code=404):
-        super().__init__(message, code)
+    def __init__(self, message="No Records Found"):
+        super().__init__(message)
