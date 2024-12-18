fd821c5bdea89eacd39692d4d76fdb55b8b3a5ed
Sam Malikin <workmalikin@gmail.com>
2024-12-18 21:10:27 +0200

    Update get_regions.py

--- app/controllers/accommodation_controllers/location_controller/region_controller/methods/get_regions.py
+++ app/controllers/accommodation_controllers/location_controller/region_controller/methods/get_regions.py
@@ -1,4 +1,3 @@
-import logging
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from typing import List
@@ -6,35 +5,20 @@ from typing import List
 from models import Region
 from utils import Finder
 from utils.error_handler import ValidationError, NoRecordsFound
+from .parse_full_region import parse_full_region
+from utils.logs_handler import log_info
 
-ERR_MSG = "Error occurred while fetching Region records"
-TRACEBACK = True
 
-
-def get_regions(session: Session) -> List[Region]:
+def get_regions(session: Session) -> List:
+    log_info('Regionss fetching started')
     try:
         regions = Finder.fetch_records(session, Region)
         Finder.log_found_amount(regions)
-        return regions
 
-    except NoRecordsFound as e:
-        logging.warning(e, exc_info=TRACEBACK)
-        raise
+        result = [parse_full_region(region) for region in regions]
 
-    except ValidationError as e:
-        logging.error(
-            f"Validation {ERR_MSG}: {e}", exc_info=TRACEBACK
-        )
-        raise
+        log_info('Regions fetching successfully finished')
+        return result
 
-    except SQLAlchemyError as e:
-        logging.error(
-            f"Data Base {ERR_MSG}: {e}", exc_info=TRACEBACK
-        )
-        raise
-
-    except Exception as e:
-        logging.error(
-            f"Unexpected {ERR_MSG}: {e}", exc_info=TRACEBACK
-        )
+    except (NoRecordsFound, ValidationError, SQLAlchemyError, Exception):
         raise
