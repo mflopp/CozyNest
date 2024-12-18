c9313def1256ee92018e8365a2b9681a8a943be3
Sam Malikin <workmalikin@gmail.com>
2024-12-18 02:35:11 +0200

    Update create.py

--- app/routes/country_routes/create.py
+++ app/routes/country_routes/create.py
@@ -1,4 +1,3 @@
-import logging
 from flask import request, Response
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -9,57 +8,39 @@ from .countries_blueprint import country_bp
 from utils import create_response
 from config import session_scope
 
+from utils.logs_handler import log_err
+
 ERR_MSG = 'Error occurred while creating Country record'
-TRACEBACK = True
 
 
 @country_bp.route("", methods=['POST'])
 def create_country_handler() -> Response:
     try:
         request_data = request.get_json()
+
         if not request_data:
-            return create_response(
-                data=[("error", "Request body is required")],
-                code=400
-            )
+            raise ValueError("Request body is required")
 
         with session_scope() as session:
             country = CountryController.create(request_data, session)
-            if country:
-                return create_response(
-                    data=[("country", country)],
-                    code=200
-                )
 
             return create_response(
-                data=[("message", "Country not created")],
-                code=400
+                data=[("country", country)],
+                code=200
             )
 
     except ValidationError as e:
-        logging.error(f"Validation {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+        log_err(f"Validation {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=400)
 
     except ValueError as e:
-        logging.error(f"Value {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+        log_err(f"Value {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=400)
 
     except SQLAlchemyError as e:
-        logging.error({f"Data Base {ERR_MSG}: {e}"}, exc_info=TRACEBACK)
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+        log_err(f"Data Base {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=500)
 
     except Exception as e:
-        logging.error(f"Unexpected {ERR_MSG}: {str(e)}", exc_info=TRACEBACK)
-        return create_response(
-            data=[("error", "Error creating country data")],
-            code=500
-        )
+        log_err(f"Unexpected {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", "Creating error")], code=500)
