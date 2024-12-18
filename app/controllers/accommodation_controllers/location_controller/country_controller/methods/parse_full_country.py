fb934aa6fd9efc9f3c7574d1db2c6b2559f826ca
Sam Malikin <workmalikin@gmail.com>
2024-12-18 20:22:22 +0200

    Update parse_full_country.py

--- app/controllers/accommodation_controllers/location_controller/country_controller/methods/parse_full_country.py
+++ app/controllers/accommodation_controllers/location_controller/country_controller/methods/parse_full_country.py
@@ -1,9 +1,10 @@
-from typing import Type, Any, Dict
+from typing import Dict
+from models import Country
 
 from utils.record_handler import Recorder
 
 
-def parse_full_country(country: Type[Any]) -> Dict:
+def parse_full_country(country: Country) -> Dict:
     parsed_country = Recorder.parse(
         model_instance=country,
         include_fields=['id', 'name']
