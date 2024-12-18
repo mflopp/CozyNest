0059e1aa5606836d44f7fefe83186724a041da90
Sam Malikin <workmalikin@gmail.com>
2024-12-18 02:35:08 +0200

    Update parse_full_region.py

--- app/controllers/accommodation_controllers/location_controller/region_controller/methods/parse_full_region.py
+++ app/controllers/accommodation_controllers/location_controller/region_controller/methods/parse_full_region.py
@@ -1,13 +1,11 @@
 from typing import Type, Any, Dict
 
 from utils.record_handler import Recorder
+from ...country_controller import CountryController
 
 
 def parse_full_region(region: Type[Any]) -> Dict:
-    parsed_country = Recorder.parse(
-        model_instance=region.country,
-        include_fields=['id', 'name']
-    )
+    parsed_country = CountryController.parse_full(region.country)
 
     parsed_region = Recorder.parse(
         model_instance=region,
