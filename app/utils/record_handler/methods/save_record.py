2ac547efd0d36471138e33c40e168d27bf94977e
Sam Malikin <workmalikin@gmail.com>
2024-12-18 02:35:39 +0200

    Update save_record.py

--- app/utils/record_handler/methods/save_record.py
+++ app/utils/record_handler/methods/save_record.py
@@ -1,16 +1,18 @@
-import logging
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from typing import Type, Any
 
+from utils.logs_handler import log_info, log_err
+
 
 def save_record(session: Session, record: Type[Any]) -> None:
+    log_info('Saving record started')
     try:
         with session.begin_nested():
             session.add(record)
             session.flush()
 
-        logging.info("Record saved to database session successfully.")
+        log_info("Record saved to database session successfully.")
     except SQLAlchemyError as e:
-        logging.error(f"Failed to save record: {str(e)}")
+        log_err(f"Failed to save record: {str(e)}")
         raise e
