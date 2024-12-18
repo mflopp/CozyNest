4da6574e44bb6ca3f0eab62ca6b893b4aecdaae8
Sam Malikin <workmalikin@gmail.com>
2024-12-18 20:22:35 +0200

    Update update.py

--- app/routes/country_routes/update.py
+++ app/routes/country_routes/update.py
@@ -1,27 +1,24 @@
-import logging
 from flask import Response, request
 from sqlalchemy.exc import SQLAlchemyError
 
 from controllers import CountryController
 from utils.error_handler import ValidationError, NoRecordsFound
-
+from utils.logs_handler import log_err
 from utils import create_response
 from config import session_scope
 from .countries_blueprint import country_bp
+
+ERR_MSG = 'Error occurred while updating Country record'
 
 
 @country_bp.route("/<int:id>", methods=['PUT'])
 def update_country_handler(id: int) -> Response:
     try:
-        # Extract new data from the request
         new_data = request.get_json()
-        if not new_data:
-            return create_response(
-                data=[("error", "Request body is required")],
-                code=400
-            )
 
-        # Using session_scope context manager for database session
+        if not new_data:
+            raise ValueError("Request body is required")
+
         with session_scope() as session:
             CountryController.update(id, new_data, session)
             return create_response(
@@ -29,52 +26,22 @@ def update_country_handler(id: int) -> Response:
                 code=200
             )
 
-    except ValidationError as e:
-        # Logging validation error
-        logging.error(f"Validation Error occurred while updating: {str(e)}")
-
-        # Returning validation error response
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
-
     except ValueError as e:
-        logging.error(
-            f"Value Error occured while updating: {str(e)}",
-            exc_info=True
-        )
-        # Returning validation error response
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+        log_err(f"Value {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=400)
 
     except NoRecordsFound as e:
-        msg = f"No records found to update with ID {id}"
-        logging.error(f"{msg}: {str(e)}", exc_info=True)
+        log_err(f"No records found to update with ID={id}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=404)
 
-        return create_response(
-            data=[("error", msg)],
-            code=404
-        )
+    except ValidationError as e:
+        log_err(f"Validation {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=409)
 
     except SQLAlchemyError as e:
-        logging.error(
-            {f"Data Base error occurred while updating: {e}"},
-            exc_info=True
-        )
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+        log_err(f"Data Base {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=500)
 
     except Exception as e:
-        # Logging unexpected error with traceback
-        logging.error(f"Error occurred while updating country data: {str(e)}")
-
-        # Returning general error response
-        return create_response(
-            data=[("error", "Error updating country data")],
-            code=500
-        )
+        log_err(f"Unexpected {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", "Updating error")], code=500)
