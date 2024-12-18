dff3f1ba837a4dd354a8c01e63deab1a3f93d65a
Sam Malikin <workmalikin@gmail.com>
2024-12-18 02:35:42 +0200

    Update create_response.py

--- app/utils/response_handler/create_response.py
+++ app/utils/response_handler/create_response.py
@@ -1,9 +1,11 @@
 import json
 from collections import OrderedDict
 from flask import Response, make_response
+from utils.logs_handler import log_info
 
 
 def create_response(data, code) -> Response:
+    log_info('Creating response started!')
     response_data = OrderedDict(data)
     response_json = json.dumps(
         response_data,
@@ -13,4 +15,5 @@ def create_response(data, code) -> Response:
     response = make_response(response_json, code)
     response.headers['Content-Type'] = 'application/json'
 
+    log_info(f'Response successfully created: {response}')
     return response
