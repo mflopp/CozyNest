50b7ef8e8ecc6b9afa9c0f11552cea13a06a20a5
Sam Malikin <workmalikin@gmail.com>
2024-12-18 20:22:53 +0200

    Update validate_unique_fields.py

--- app/utils/validate_handler/methods/validate_unique_fields.py
+++ app/utils/validate_handler/methods/validate_unique_fields.py
@@ -1,9 +1,9 @@
-import logging
 from sqlalchemy.orm import Session
 from typing import Type, Dict, Any
 
 from utils import Finder
 from utils.error_handler import ValidationError
+from utils.logs_handler import log_info, log_err
 
 
 def validate_uniqueness(
@@ -12,7 +12,7 @@ def validate_uniqueness(
     criteria: Dict[str, Any]
 ) -> None:
     try:
-        logging.info("Validation of unique values started")
+        log_info("Validation of unique values started")
 
         record = Finder.fetch_record(
             session=session,
@@ -22,10 +22,10 @@ def validate_uniqueness(
 
         if record:
             msg = f"Duplicate record found with values: {criteria}."
-            logging.error(f"validate_uniqueness(): {msg}", exc_info=True)
+            log_err(f"validate_uniqueness(): {msg}")
             raise ValidationError(msg)
 
-        logging.info("Validation of unique values passed")
+        log_info("Validation of unique values passed")
 
     except ValidationError:
         raise
