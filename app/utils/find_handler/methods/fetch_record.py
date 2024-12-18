b4bc0541af2d4d85a558e06f0002e79fdaf549e6
Sam Malikin <workmalikin@gmail.com>
2024-12-18 02:35:25 +0200

    Update fetch_record.py

--- app/utils/find_handler/methods/fetch_record.py
+++ app/utils/find_handler/methods/fetch_record.py
@@ -1,8 +1,8 @@
-import logging
 from sqlalchemy.orm import Session
-from typing import Any, Type, Dict
-
 from sqlalchemy.exc import SQLAlchemyError
+from typing import Type, Any, Dict
+
+from utils.logs_handler import log_info
 
 
 def fetch_one(
@@ -10,24 +10,22 @@ def fetch_one(
     Model: Type[Any],
     criteria: Dict[str, Any]
 ) -> Any:
+    log_info("Record fetching started")
     try:
-        logging.info("Record fetching started...")
-        # Get the name of the model for logging purposes
         model_name = Model.__name__
 
         record = session.query(Model).filter_by(**criteria).first()
 
         if not record:
-            logging.info(f'No {model_name} record found where: {criteria}')
+            log_info(f'No {model_name} record found where: {criteria}')
             return None
 
         # Log the ID of the fetched record if it exists
-        logging.info(f"{model_name} found with ID {record.id}")
+        log_info(f"{model_name} found with ID = {record.id}")
         return record
 
-    except SQLAlchemyError as e:
-        # Improved logging with detailed database context
-        data = f"{model_name} with '{criteria}': {e}"
-        msg = f"Error occurred while fetching: {data}."
-        # Raise exception to handle upstream
-        raise SQLAlchemyError(msg)
+    except SQLAlchemyError:
+        raise
+        # data = f"{model_name} with '{criteria}': {e}"
+        # msg = f"Error occurred while fetching: {data}."
+        # raise SQLAlchemyError(msg)
