6227e6946869b0762962f5260ee6169919073bbb
Sam Malikin <workmalikin@gmail.com>
2024-12-18 02:35:14 +0200

    Update get_all.py

--- app/routes/country_routes/get_all.py
+++ app/routes/country_routes/get_all.py
@@ -1,4 +1,3 @@
-import logging
 from flask import Response
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -6,7 +5,11 @@ from controllers import CountryController
 from utils.error_handler import NoRecordsFound, ValidationError
 from utils import create_response
 from config import session_scope
+from utils.logs_handler import log_err
+
 from .countries_blueprint import country_bp
+
+ERR_MSG = 'Error occurred while fetching Country records'
 
 
 @country_bp.route("", methods=['GET'])
@@ -15,62 +18,27 @@ def get_countries_handler() -> Response:
         with session_scope() as session:
             countries = CountryController.get_all(session)
 
-            if countries:
-                return create_response(
-                    data=[("countries", countries)],
-                    code=200
-                )
-
             return create_response(
-                data=[("message", "Countries not found")],
-                code=404
+                data=[("countries", countries)],
+                code=200
             )
 
-    except ValidationError as e:
-        msg = "Validation Error occurred while fetching countries"
-        logging.error(f"{msg}: {str(e)}", exc_info=True)
-
-        return create_response(
-            data=[("error", msg)],
-            code=409
-        )
+    except ValueError as e:
+        log_err(f"Value {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=400)
 
     except NoRecordsFound as e:
-        msg = "No records found while fetching countries"
-        logging.error(f"{msg}: {str(e)}", exc_info=True)
+        log_err(f"{ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=404)
 
-        return create_response(
-            data=[("error", msg)],
-            code=404
-        )
-
-    except ValueError as e:
-        logging.error(
-            f"Value Error occured while fetching: {str(e)}",
-            exc_info=True
-        )
-
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+    except ValidationError as e:
+        log_err(f'Validation {ERR_MSG}: {str(e)}')
+        return create_response(data=[("error", str(e))], code=409)
 
     except SQLAlchemyError as e:
-        logging.error(
-            {f"Data Base error occurred while fetching: {e}"},
-            exc_info=True
-        )
-
-        return create_response(
-            data=[("error", str(e))],
-            code=400
-        )
+        log_err(f"Data Base {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", str(e))], code=500)
 
     except Exception as e:
-        msg = "An unexpected error occurred while fetching"
-        logging.error(f"{msg}: {str(e)}", exc_info=True)
-
-        return create_response(
-            data=[("error", msg)],
-            code=500
-        )
+        log_err(f"Unexpected {ERR_MSG}: {str(e)}")
+        return create_response(data=[("error", "Fetching error")], code=500)
