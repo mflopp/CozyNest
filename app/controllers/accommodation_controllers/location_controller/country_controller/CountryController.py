dcceb7332ea570f389d511e319418ec6dc9bca50
Sam Malikin <workmalikin@gmail.com>
2024-12-18 02:34:42 +0200

    Update CountryController.py

--- app/controllers/accommodation_controllers/location_controller/country_controller/CountryController.py
+++ app/controllers/accommodation_controllers/location_controller/country_controller/CountryController.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Type, List, Dict, Any
 from sqlalchemy.orm import Session
 from models import Country
 
@@ -7,26 +7,28 @@ from .methods import (
     delete_country,
     get_country,
     get_countries,
-    update_country
+    update_country,
+    parse_full_country
 )
 
 
 class CountryController:
     @staticmethod
-    def create(data: dict, session: Session) -> Country:
+    def create(data: dict, session: Session) -> Dict[str, Any]:
         country = create_country(data, session)
         session.commit()
         return country
 
     @staticmethod
-    def get_country(country_id: int, session: Session) -> Country:
-        contry = get_country(country_id, session)
-        return contry
+    def get_country(
+        country_id: int, session: Session, return_instance: bool = False
+    ) -> Country | Dict[str, Any]:
+        country = get_country(country_id, session, return_instance)
+        return country
 
     @staticmethod
     def get_all(session: Session) -> List:
         countries = get_countries(session)
-        session.commit()
         return countries
 
     @staticmethod
@@ -38,4 +40,9 @@ class CountryController:
     def update(country_id: int, data: Dict, session: Session) -> Country:
         result = update_country(country_id, data, session)
         session.commit()
+        return result
+
+    @staticmethod
+    def parse_full(country: Type[Any]) -> Dict[str, Any]:
+        result = parse_full_country(country)
         return result
