6f90eef53dc6cb6e4743c9bc08d46146d90f5a75
Alexander Kor <megaflopp@gmail.com>
2024-12-19 14:53:07 +0200

    Update delete_amenity_category.py

--- app/controllers/accommodation_controllers/amenity_category_controller/methods/delete_amenity_category.py
+++ app/controllers/accommodation_controllers/amenity_category_controller/methods/delete_amenity_category.py
@@ -1,44 +1,49 @@
-import logging
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
-from models.accommodations import Amenity
+from models.accommodations import Amenity, AmenitiesCategory
 
-from .get_amenity_category import fetch_amenity_category
 from utils import Recorder
 from utils.error_handler import (
+    ValidationError,
     NoRecordsFound,
     HasChildError
 )
 
+from utils.logs_handler import log_err, log_info
+from .get_amenity_category import fetch_amenity_category
+
 
 def del_amenity_category(id: int, session: Session):
-
+    log_info(f"Amenity category with ID={id} deletion started...")
     try:
 
         # Start a new transaction
         with session.begin_nested():
 
             # Getting gender by id from the DB
-            category = fetch_amenity_category('id', id, session, True)
+            category: AmenitiesCategory = fetch_amenity_category(
+                'id', id, session, True
+            )
 
-            if not category:
-                msg = f"Amenity category ID {id} not found"
-                raise NoRecordsFound(msg)
+            # if not category:
+            #     msg = f"Amenity category ID {id} not found"
+            #     raise NoRecordsFound(msg)
 
             # Check if current category is in use in user_info
-            if not Recorder.has_child(category, Amenity):
-                # Attempt to delete the record
-                Recorder.delete(session, category)
-            else:
-                msg = (
-                    f"Impossible to delete amenity category with ID {id}. "
-                    "Record has child records"
+            if Recorder.has_child(category, Amenity):
+                log_err(
+                    f"del_amenity_category(): Deletion {id} forbidden"
+                    f"- {category} has associations in Amenity"
                 )
-                raise HasChildError(msg)
+                raise HasChildError
+
+            # Attempt to delete the record
+            Recorder.delete(session, category)
 
         # commit the transaction after 'with' block
         session.flush()
-        logging.info(f"Amenity category ID {id} deleted successfully")
+        log_info(f"Amenity category ID {id} deletion successfully finished")
 
-    except (NoRecordsFound, HasChildError, SQLAlchemyError, Exception):
+    except (NoRecordsFound, ValueError, HasChildError,
+            ValidationError, SQLAlchemyError, Exception):
         raise
