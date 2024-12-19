c449f785677e00ba798b00d1f803e6b6ef8db0f9
Alexander Kor <megaflopp@gmail.com>
2024-12-19 14:05:42 +0200

    Update delete_amenity_category.py

--- app/routes/amenity_categories_routes/delete_amenity_category.py
+++ app/routes/amenity_categories_routes/delete_amenity_category.py
@@ -1,77 +1,26 @@
-import logging
-from sqlalchemy.exc import SQLAlchemyError
-
+from flask import Response
 from controllers import AmenityCategoryController
-from .amenity_categories_blueprint import amenity_categories_bp
-from utils.error_handler import (
-    NoRecordsFound,
-    HasChildError
-)
 
+from utils.exceptions_handler import crud_exceptions_handler
 from utils import create_response
 from config import session_scope
 
+from .amenity_categories_blueprint import amenity_categories_bp
+
+ERR_MSG = 'Error occurred while deleting Amenity category record'
+
 
 @amenity_categories_bp.route("/<int:id>", methods=['DELETE'])
-def delete_amenity_category_handler(id: int) -> tuple:
-    """
-    Endpoint for deleting a gender by ID. Attempts to delete
-    the gender from the database and returns a response indicating
-    success or failure.
+@crud_exceptions_handler(ERR_MSG)
+def delete_amenity_category_handler(id: int) -> Response:
 
-    Args:
-        id (int): The ID of the gender to delete.
+    # Use the session_scope context manager
+    with session_scope() as session:
+        # deleting amenity category record by id
+        AmenityCategoryController.delete(id, session)
 
-    Returns:
-        tuple: A tuple with the response and HTTP status code.
-    """
-    try:
-        # Use the session_scope context manager
-        with session_scope() as session:
-            AmenityCategoryController.delete(id, session)
-
-            return create_response(
-                data=[
-                    ("info", f"Amenity category ID {id} deleted successfully")
-                ],
-                code=200
-            )
-
-    except NoRecordsFound as e:
-        msg, code = e.args  # Unpacking the tuple
-        logging.error(f"{msg}", exc_info=True, stack_info=True, stacklevel=2)
+        # creating user response
         return create_response(
-            data=[("error", msg)],
-            code=code
-        )
-
-    except HasChildError as e:
-        msg, code = e.args  # Unpacking the tuple
-        logging.error(f"{msg}", exc_info=True, stack_info=True, stacklevel=2)
-        # Returning validation error response
-        return create_response(
-            data=[("error", msg)],
-            code=code
-        )
-
-    except SQLAlchemyError as e:
-        msg = (
-            "DB error occurred while deleting amenity category "
-            f"with ID {id}: {e}"
-        )
-        logging.error(msg, exc_info=True, stack_info=True, stacklevel=2)
-        return create_response(
-            data=[("error", msg)],
-            code=500
-        )
-
-    except Exception as e:
-        msg = (
-            "Error occurred while deleting amenity category "
-            f"with ID {id}: {str(e)}"
-        )
-        logging.error(msg, exc_info=True, stack_info=True, stacklevel=2)
-        return create_response(
-            data=[("error", msg)],
-            code=500
+            data=[("info", f"Amenity category ID {id} deleted successfully")],
+            code=200
         )
