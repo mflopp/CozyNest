2c4b71dc4e62c427cc211dbad52ad58ca1f9a352
Alexander Kor <megaflopp@gmail.com>
2024-12-19 13:52:11 +0200

    Update get_all.py

--- app/controllers/accommodation_controllers/amenity_category_controller/methods/get_all.py
+++ app/controllers/accommodation_controllers/amenity_category_controller/methods/get_all.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from typing import List
 
-# from utils.error_handler import ValidationError, NoRecordsFound
+from utils.error_handler import ValidationError, NoRecordsFound
 from models.accommodations import AmenitiesCategory
 from utils import Finder
 from .parse_full_amenity_category import parse_full_amenity_category
@@ -10,11 +10,11 @@ from utils.logs_handler import log_info
 
 
 def fetch_amenity_categories(session: Session) -> List:
-
+    log_info('Amenity categories fetching started...')
     try:
         # Fetch all genders records
         categories = Finder.fetch_records(session, AmenitiesCategory)
-        # Finder.log_found_amount(categories)
+        Finder.log_found_amount(categories)
 
         result = [
             parse_full_amenity_category(category) for category in categories
@@ -24,5 +24,5 @@ def fetch_amenity_categories(session: Session) -> List:
 
         return result
 
-    except (SQLAlchemyError, Exception):
+    except (NoRecordsFound, ValidationError, SQLAlchemyError, Exception):
         raise
