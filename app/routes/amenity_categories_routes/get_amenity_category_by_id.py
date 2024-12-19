e6d1c269d8c6e80f2183c020acb62a7c528874d8
Alexander Kor <megaflopp@gmail.com>
2024-12-19 14:05:53 +0200

    Update get_amenity_category_by_id.py

--- app/routes/amenity_categories_routes/get_amenity_category_by_id.py
+++ app/routes/amenity_categories_routes/get_amenity_category_by_id.py
@@ -1,60 +1,30 @@
-import logging
-from sqlalchemy.exc import SQLAlchemyError
-
+from flask import Response
 from controllers import AmenityCategoryController
-from .amenity_categories_blueprint import amenity_categories_bp
 
+from utils.exceptions_handler import crud_exceptions_handler
 from utils import create_response
 from config import session_scope
 
+from .amenity_categories_blueprint import amenity_categories_bp
+
+ERR_MSG = 'Error occurred while fetching Amenity category record'
+
 
 @amenity_categories_bp.route("<int:id>", methods=['GET'])
-def get_amenity_category_by_id_handler(id: int):
-    """
-    Endpoint for retrieving a user setting by ID. Looks up the user
-    in the database and returns the user setting data or a 404 error
-    if not found.
-
-    Args:
-        id (int): The ID of the user setting to retrieve.
-
-    Returns:
-        tuple: A tuple with the response and HTTP status code.
-    """
-    try:
-        # Use the session_scope context manager
-        with session_scope() as session:
-
-            amenity_category = AmenityCategoryController.get_one_by_id(
-                id, session
-            )
-
-            if amenity_category:
-                return create_response(
-                    data=[
-                        ("amenity category", amenity_category)
-                        # ("id", amenity_category.id),
-                        # ("categoty", amenity_category.category)
-                    ],
-                    code=200
-                )
-
-            return {"message": f"Amenity category with ID {id} not found"}, 404
-
-    except SQLAlchemyError as e:
-        msg = f"DB error occured while fetching an amenity category: {str(e)}"
-        logging.error(msg, exc_info=True, stack_info=True, stacklevel=2)
-        return create_response(
-            data=[("error", f"{str(e)}")],
-            code=500
+@crud_exceptions_handler(ERR_MSG)
+def get_amenity_category_by_id_handler(id: int) -> Response:
+    
+    # Use the session_scope context manager
+    with session_scope() as session:
+        # fetching the amenity category by id
+        amenity_category = AmenityCategoryController.get_one_by_id(
+            id, session
         )
 
-    except Exception as e:
-        msg = (
-            f"Unexpected error while getting an amenity category ID {id}: {e}"
-        )
-        logging.error(msg, exc_info=True, stack_info=True, stacklevel=2)
+        # creating user response
         return create_response(
-            data=[("error", msg)],
-            code=500
+            data=[
+                ("amenity category", amenity_category)
+            ],
+            code=200
         )
