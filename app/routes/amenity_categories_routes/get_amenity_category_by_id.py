eef9c453ebcfc52ae839dffe71fe50a50afa26c7
Alexander Kor <megaflopp@gmail.com>
2024-12-19 19:39:21 +0200

    Update get_amenity_category_by_id.py

--- app/routes/amenity_categories_routes/get_amenity_category_by_id.py
+++ app/routes/amenity_categories_routes/get_amenity_category_by_id.py
@@ -10,7 +10,9 @@ from .amenity_categories_blueprint import amenity_categories_bp
 ERR_MSG = 'Error occurred while fetching Amenity category record'
 
 
-@amenity_categories_bp.route("<int:id>", methods=['GET'])
+@amenity_categories_bp.route(
+    "<int:id>", methods=['GET'], endpoint='get_amenity_category_by_id'
+)
 @crud_exceptions_handler(ERR_MSG)
 def get_amenity_category_by_id_handler(id: int) -> Response:
 
