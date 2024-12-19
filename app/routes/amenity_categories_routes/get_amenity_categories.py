d2c4401911d9f5593642ec4eb731ad65da32775c
Alexander Kor <megaflopp@gmail.com>
2024-12-19 13:52:14 +0200

    Update get_amenity_categories.py

--- app/routes/amenity_categories_routes/get_amenity_categories.py
+++ app/routes/amenity_categories_routes/get_amenity_categories.py
@@ -1,50 +1,25 @@
 from flask import Response
-import logging
-from sqlalchemy.exc import SQLAlchemyError
 
 from controllers import AmenityCategoryController
-from .amenity_categories_blueprint import amenity_categories_bp
-# from utils.error_handler import NoRecordsFound, ValidationError
 
-# from utils.logs_handler import log_err
+from utils.exceptions_handler import crud_exceptions_handler
 from utils import create_response
 from config import session_scope
 
+from .amenity_categories_blueprint import amenity_categories_bp
+
+ERR_MSG = 'Error occurred while fetching Amenity categories record'
+
 
 @amenity_categories_bp.route("", methods=['GET'])
+@crud_exceptions_handler(ERR_MSG)
 def get_amenity_categories_handler() -> Response:
-    """
-    Endpoint for retrieving all user settings from the database.
-    Returns a list of user settings or an error message if not found.
+    # Use the session_scope context manager
+    with session_scope() as session:
+        amenity_categories = AmenityCategoryController.get_all(session)
 
-    Returns:
-        tuple: A tuple with the response and HTTP status code.
-    """
-    try:
-        # Use the session_scope context manager
-        with session_scope() as session:
-            amenity_categories = AmenityCategoryController.get_all(session)
-
-            if amenity_categories:
-                return create_response(
-                    data=[("amenity categories", amenity_categories)],
-                    code=200
-                )
-
-            return {"message": "Amenity categories not found"}, 404
-
-    except SQLAlchemyError as e:
-        msg = f"DB error occured while fetching amenity categories: {str(e)}"
-        logging.error(msg, exc_info=True, stack_info=True, stacklevel=2)
-        return create_response(
-            data=[("error", msg)],
-            code=500
-        )
-
-    except Exception as e:
-        msg = f"Error occurred while fetching amenity categories: {str(e)}"
-        logging.error(msg, exc_info=True, stack_info=True, stacklevel=2)
-        return create_response(
-            data=[("error", msg)],
-            code=500
-        )
+        if amenity_categories:
+            return create_response(
+                data=[("amenity categories", amenity_categories)],
+                code=200
+            )
