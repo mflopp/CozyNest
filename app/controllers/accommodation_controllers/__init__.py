32f0806d69f3b8eba8e5a843d262b2e4f4ee87a0
Alexander Kor <megaflopp@gmail.com>
2024-12-20 21:47:59 +0200

    Update __init__.py

--- app/controllers/accommodation_controllers/__init__.py
+++ app/controllers/accommodation_controllers/__init__.py
@@ -3,6 +3,7 @@ from .location_controller import RegionController
 from .location_controller import CityController
 from .amenity_category_controller import AmenityCategoryController
 from .amenity_controller import AmenityController
+from .accommodation_amenity_controller import AccommodationAmenityController
 
 
 __all__ = [
@@ -10,5 +11,6 @@ __all__ = [
     'RegionController',
     'CityController',
     'AmenityCategoryController',
-    'AmenityController'
+    'AmenityController',
+    'AccommodationAmenityController'
 ]
