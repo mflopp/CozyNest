3b0a2f0ec2c9159d692136eb1cce259e87c4ab32
Alexander Kor <megaflopp@gmail.com>
2024-12-20 21:48:23 +0200

    Create parse_full_accommodation_amenity.py

---
+++ app/controllers/accommodation_controllers/accommodation_amenity_controller/methods/parse_full_accommodation_amenity.py
@@ -0,0 +1,24 @@
+from typing import Type, Any, Dict
+
+from utils.record_handler import Recorder
+from ...amenity_controller import AmenityController
+# from ...accommodation_controller import AmenityController
+
+
+def parse_full_accommodation_amenity(accommodation_amenity: Type[Any]) -> Dict:
+    parsed_amenity = AmenityController.parse_full(
+        accommodation_amenity.amenity
+    )
+    # parsed_accommodation = accommodationController.parse_full(
+    #     accommodation_amenity.accommodation
+    # )
+
+    parsed_accommodation_amenity = Recorder.parse(
+        model_instance=accommodation_amenity,
+        include_fields=['id']
+    )
+
+    parsed_accommodation_amenity['amenity_id'] = parsed_amenity
+    # parsed_accommodation_amenity['accommodation_id'] = parsed_accommodation
+
+    return parsed_accommodation_amenity
