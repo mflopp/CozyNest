7cb440c355e050d97397e634f21f1e0471b705f0
Sam Malikin <workmalikin@gmail.com>
2024-12-20 16:37:19 +0200

    Update __init__.py

--- app/controllers/accommodation_controllers/location_controller/city_controller/methods/__init__.py
+++ app/controllers/accommodation_controllers/location_controller/city_controller/methods/__init__.py
@@ -2,10 +2,14 @@ from .create_city import create_city
 from .get_city import get_city
 from .get_cities import get_cities
 from .delete_city import delete_city
+from .update_city import update_city
+from .parse_full_city import parse_full_city
 
 __all__ = [
     'create_city',
     'get_city',
     'get_cities',
     'delete_city',
+    'update_city',
+    'parse_full_city'
 ]
