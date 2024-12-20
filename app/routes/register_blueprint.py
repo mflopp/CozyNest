809e6146195efe05ebbb69985ac9969ff0e09310
Alexander Kor <megaflopp@gmail.com>
2024-12-20 21:49:02 +0200

    Update register_blueprint.py

--- app/routes/register_blueprint.py
+++ app/routes/register_blueprint.py
@@ -8,6 +8,7 @@ from .user_info_routes import user_info_bp
 from .user_routes import users_bp
 from .amenity_categories_routes import amenity_categories_bp
 from .amenities_routes import amenities_bp
+from .accommodation_amenities_routes import accommodation_amenities_bp
 
 
 def register_all_blueprints(app):
@@ -21,3 +22,4 @@ def register_all_blueprints(app):
     app.register_blueprint(users_bp)
     app.register_blueprint(amenity_categories_bp)
     app.register_blueprint(amenities_bp)
+    app.register_blueprint(accommodation_amenities_bp)
