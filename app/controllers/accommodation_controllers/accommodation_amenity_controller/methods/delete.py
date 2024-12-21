580576a0ec161012026403b5109c5378fbea281d
Alexander Kor <megaflopp@gmail.com>
2024-12-21 09:18:35 +0200

    Update delete.py

--- app/controllers/accommodation_controllers/accommodation_amenity_controller/methods/delete.py
+++ app/controllers/accommodation_controllers/accommodation_amenity_controller/methods/delete.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
-from models.accommodations import Amenity, AccommodationAmenity, Accommodation
+from models.accommodations import AccommodationAmenity
 
 from utils import Recorder
 from utils.error_handler import (
@@ -9,7 +9,7 @@ from utils.error_handler import (
     HasChildError
 )
 
-from utils.logs_handler import log_err, log_info
+from utils.logs_handler import log_info
 from .get_one import fetch_accommodation_amenity
 
 
@@ -27,23 +27,6 @@ def del_accommodation_amenity(id: int, session: Session):
                 session,
                 True
             )
-
-            # Check if accommodation_amenity is in use in Amenity
-            if Recorder.has_child(acc_amenity, Amenity):
-                log_err(
-                    f"del_accommodation_amenity(): Deletion {id} forbidden "
-                    f"- {acc_amenity} has associations in Amenity"
-                )
-                raise HasChildError
-
-            # Check if accommodation_amenity is in use in Accommodation
-            if Recorder.has_child(acc_amenity, Accommodation):
-                log_err(
-                    f"del_accommodation_amenity(): Deletion {id} forbidden "
-                    f"- {acc_amenity} has associations "
-                    "in Accommodation"
-                )
-                raise HasChildError
 
             # Attempt to delete the record
             Recorder.delete(session, acc_amenity)
